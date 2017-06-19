a224f4ba0734e316dd6d8851af9edcbc36560b94
zhongyg <zhongyg@zhongyg-note.kr.ad>
2017-06-19 10:52:15 +0800

    public 封装 getput等方法 Signed-off-by:zhongyg <zhongyg@zhongyg-note.kr.ad>

--- application/controllers/Public.php
+++ application/controllers/Public.php
@@ -70,10 +70,151 @@ abstract class PublicController extends Yaf_Controller_Abstract {
     }
 
     protected function jsonReturn($data, $type = 'JSON') {
-
-
         header('Content-Type:application/json; charset=utf-8');
-        exit(json_encode($data,JSON_UNESCAPED_UNICODE));
+        exit(json_encode($data, JSON_UNESCAPED_UNICODE));
+    }
+
+    /*
+     *  获取put过来的数据
+     * @param string $name
+     * @param mixed $default
+     *
+     * @return mixed
+     */
+
+    public function getPut($name, $default = null) {
+        $data = isset($this->put_data [$name]) ? $this->put_data [$name] : $default;
+        // return array_walk_recursive($data, 'think_filter');
+        return $data;
+    }
+
+    /**
+     *
+     * @link http://www.php.net/manual/en/yaf-request-abstract.getparam.php
+     *
+     * @param string $name
+     * @param mixed $default
+     *
+     * @return mixed
+     */
+    public function getParam($name, $default = null) {
+
+        return $this->getRequest()->getParam($name, $default);
+    }
+
+    /**
+     * Retrieve variable from client, this method will search the name in $_REQUEST params, if the name is not found, then will search in $_POST, $_GET, $_COOKIE, $_SERVER
+     *
+     * @link http://www.php.net/manual/en/yaf-request-http.get.php
+     *
+     * @param string $name the variable name
+     * @param string $default if this parameter is provide, this will be returned if the variable can not be found
+     *
+     * @return mixed
+     */
+    public function get($name, $default = null) {
+
+        return $this->getRequest()->get($name, $default);
+    }
+
+    /**
+     * Retrieve $_GET variable
+     *
+     * @link http://www.php.net/manual/en/yaf-request-http.getquery.php
+     *
+     * @param string $name the variable name, if not provided returns all
+     * @param mixed $default if this parameter is provide, this will be returned if the variable can not be found
+     *
+     * @return mixed
+     */
+    public function getQuery($name, $default = null) {
+
+        return $this->getRequest()->getQuery($name, $default);
+    }
+
+    /**
+     * Retrieve $_POST variable
+     *
+     * @link http://www.php.net/manual/en/yaf-request-http.getpost.php
+     *
+     * @param string $name the variable name, if not provided returns all
+     * @param mixed $default if this parameter is provide, this will be returned if the variable can not be found
+     *
+     * @return mixed
+     */
+    public function getPost($name, $default = null) {
+
+        return $this->getRequest()->getPost($name, $default);
+    }
+
+    /**
+     * @link http://www.php.net/manual/en/yaf-request-abstract.getmethod.php
+     *
+     * @return string
+     */
+    public function getMethod() {
+        return $this->getRequest()->getMethod();
+    }
+
+    /**
+     * Retrieve $_SERVER variable
+     *
+     * @link http://www.php.net/manual/en/yaf-request-abstract.getserver.php
+     *
+     * @param string $name the variable name, if not provided returns all
+     * @param mixed $default if this parameter is provide, this will be returned if the variable can not be found
+     *
+     * @return mixed
+     */
+    public function getServer($name, $default = null) {
+
+        return $this->getRequest()->getServer($name, $default);
+    }
+
+    /**
+     * Retrieve $_COOKIE variable
+     *
+     * @link http://www.php.net/manual/en/yaf-request-http.getcookie.php
+     *
+     * @param string $name the variable name, if not provided returns all
+     * @param mixed $default if this parameter is provide, this will be returned if the variable can not be found
+     *
+     * @return mixed
+     */
+    public function getCookie($name, $default = null) {
+
+        return $this->getRequest()->getCookie($name, $default);
+    }
+
+    /**
+     * Retrieve $_FILES variable
+     *
+     * @link http://www.php.net/manual/en/yaf-request-http.getfiles.php
+     *
+     * @param string $name the variable name, if not provided returns all
+     * @param mixed $default if this parameter is provide, this will be returned if the variable can not be found
+     *
+     * @return mixed
+     */
+    public function getFiles($name, $default = null) {
+
+        return $this->getRequest()->getCookie($name, $default);
+    }
+
+    function think_filter(&$value) {
+        // TODO 其他安全过滤
+        // 过滤查询特殊字符
+        if (preg_match('/^(EXP|NEQ|GT|EGT|LT|ELT|OR|XOR|LIKE|NOTLIKE|NOT BETWEEN|NOTBETWEEN|BETWEEN|NOTIN|NOT IN|IN)$/i', $value)) {
+            $value .= ' ';
+        }
+    }
+
+    function array_map_recursive($filter, $data) {
+        $result = array();
+        foreach ($data as $key => $val) {
+            $result[$key] = is_array($val) ? array_map_recursive($filter, $val) : call_user_func($filter, $val);
+        }
+        return $result;
     }
 
     /**
