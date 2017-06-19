1209b3f59e1f5590be27af5d552291e25947cfe1
liujf <git@liujf>
2017-06-19 19:37:30 +0800

    *

--- application/controllers/Public.php
+++ application/controllers/Public.php
@@ -129,4 +129,60 @@ abstract class PublicController extends Yaf_Controller_Abstract {
 //    public function create($createcondition = []) {
 //
 //    }
+
+	/**
+	 * 获取生成的询价单流水号
+	 * @author liujf 2017-06-19
+	 * @return string $inquirySerialNo 询价单流水号
+	 */
+	public function getInquirySerialNo() {
+		$time = date('Ymd');
+		$duration = 3600 * 48;
+		$inquirySerialNoCreateTime = redisGet('inquirySerialNoCreateTime') ? : '19700101';
+		if ($time > $inquirySerialNoCreateTime) {
+			redisSet('inquirySerialNoStep', 0, $duration);
+			redisSet('inquirySerialNoCreateTime', $time, $duration);
+		}
+		$inquirySerialNoStep = redisGet('inquirySerialNoStep') ? : 0;
+		$inquirySerialNoStep ++;
+		redisSet('inquirySerialNoStep', $inquirySerialNoStep, $duration);
+		$inquirySerialNo = $this->createSerialNo($inquirySerialNoStep, 'INQ_');
+		
+		return $inquirySerialNo;
+	}
+	
+	/**
+	 * 获取生成的报价单流水号
+	 * @author liujf 2017-06-19
+	 * @return string $quoteSerialNo 报价单流水号
+	 */
+	public function getQuoteSerialNo() {
+		$time = date('Ymd');
+		$duration = 3600 * 48;
+		$quoteSerialNoCreateTime = redisGet('quoteSerialNoCreateTime') ? : '19700101';
+		if ($time > $quoteSerialNoCreateTime) {
+			redisSet('quoteSerialNoStep', 0, $duration);
+			redisSet('quoteSerialNoCreateTime', $time, $duration);
+		}
+		$quoteSerialNoStep = redisGet('quoteSerialNoStep') ? : 0;
+		$quoteSerialNoStep ++;
+		redisSet('inquirySerialNoStep', $quoteSerialNoStep, $duration);
+		$quoteSerialNo = $this->createSerialNo($quoteSerialNoStep, 'QUO_');
+		
+		return $quoteSerialNo;
+	}
+	
+	/**
+	 * 生成流水号
+	 * @param string $step 需要补零的字符
+	 * @param string $prefix 前缀
+	 * @author liujf 2017-06-19
+	 * @return string $code
+	 */
+	private function createSerialNo($step = 1, $prefix = '') {
+		$time = date('Ymd');
+		$pad  = str_pad($step, 5, '0', STR_PAD_LEFT);
+		$code = $prefix . $time . '_' . $pad;
+		return $code;
+	}
 }
