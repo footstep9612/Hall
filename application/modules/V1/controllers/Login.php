3703f23f552668f94c408fb678d1985c527245ee
jhw <377812705@qq.com>
2017-06-21 11:43:07 +0800

    去掉头信息

--- application/modules/V1/controllers/Login.php
+++ application/modules/V1/controllers/Login.php
@@ -1,7 +1,4 @@
 <?php
-header('Access-Control-Allow-Origin:*');
-header('Access-Control-Allow-Headers:x-requested-with,content-type');
-header('Access-Control-Allow-Methods:POST,PUT,GET');
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
