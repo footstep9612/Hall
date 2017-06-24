d1cee868fd5f9bfcd7eab342404592beef5cf9ac
konglp <konglp@qq.com>
2017-06-24 15:41:52 +0800

    k

--- application/models/GoodsAch.php
+++ application/models/GoodsAch.php
@@ -30,53 +30,50 @@ class GoodsAchModel extends PublicModel
             'status'  => self::STATUS_VALID
         );
         $field = 'attach_type,attach_name,attach_url,sort_order';
-        try {
-            $key_redis = md5(json_encode($condition));
-            if(redisExist($key_redis)){
-                $result = redisGet($key_redis);
-                return $result ? json_decode($result) : array();
-            } else {
-                $result = $this->field($field)
-                               ->where($condition)
-                               ->select();
-                if ($result) {
-                    //附件分组
-                    /**
-                     * SMALL_IMAGE-小图；
-                     * MIDDLE_IMAGE-中图；
-                     * BIG_IMAGE-大图；
-                     * DOC-文档（包括图片和各种文档类型）
-                     * */
-                    foreach ($result as $val) {
-                        $group = 'OTHERS';
-                        switch ($val['attach_type']) {
-                            case 'SMALL_IMAGE':
-                                $group = 'SMALL_IMAGE';
-                                break;
-                            case 'MIDDLE_IMAGE':
-                                $group = 'MIDDLE_IMAGE';
-                                break;
-                            case 'BIG_IMAGE':
-                                $group = 'BIG_IMAGE';
-                                break;
-                            case 'DOC':
-                                $group = 'DOC';
-                                break;
-                            default:
-                                $group = 'OTHERS';
-                                break;
-                        }
-                        $result[$group] = $val;
-                    }
 
-                    redisSet($key_redis,json_encode($result));
-                    return $result;
-                } else {
-                    return array();
+        //根据缓存读取,没有则查找数据库并缓存
+        $key_redis = md5(json_encode($condition));
+        if(redisExist($key_redis)) {
+            $result = json_decode(redisGet($key_redis));
+            return $result ? $result : array();
+        }
+        try {
+            $result = $this->field($field)->where($condition)->select();
+            if ($result) {
+                $data = array();
+                //附件分组
+                /**
+                 * SMALL_IMAGE-小图；
+                 * MIDDLE_IMAGE-中图；
+                 * BIG_IMAGE-大图；
+                 * DOC-文档（包括图片和各种文档类型）
+                 * */
+                foreach ($result as $val) {
+                    $group = 'OTHERS';
+                    switch ($val['attach_type']) {
+                        case 'SMALL_IMAGE':
+                            $group = 'SMALL_IMAGE';
+                            break;
+                        case 'MIDDLE_IMAGE':
+                            $group = 'MIDDLE_IMAGE';
+                            break;
+                        case 'BIG_IMAGE':
+                            $group = 'BIG_IMAGE';
+                            break;
+                        case 'DOC':
+                            $group = 'DOC';
+                            break;
+                        default:
+                            $group = 'OTHERS';
+                            break;
+                    }
+                    $data[$group][] = $val;
                 }
+                redisSet($key_redis,json_encode($data));
+                return $data;
             }
         }catch (Exception $e){
-            return false;
+            return array();
         }
     }
 
