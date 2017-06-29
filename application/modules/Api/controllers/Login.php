74b812b5229ed65cd97fa4988d7e558af2597a9a
jhw <377812705@qq.com>
2017-06-29 21:38:03 +0800

    修改

--- application/modules/Api/controllers/Login.php
+++ application/modules/Api/controllers/Login.php
@@ -106,11 +106,6 @@ class LoginController extends Yaf_Controller_Abstract {
             jsonReturn('',-101,'名字不能为空!');
         }
 
-        if(!empty($data['last_name'])) {
-            $buyer_account_data['last_name'] = $data['last_name'];
-        }else{
-            jsonReturn('',-101,'姓不能为空!');
-        }
         if(!empty($data['mobile'])) {
             $buyer_account_data['mobile'] = $data['mobile'];
         }
