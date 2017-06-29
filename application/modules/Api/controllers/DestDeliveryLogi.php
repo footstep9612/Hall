a506187bc3f15a60fd311c92eb769a969298b2d7
link <linkai@kr.ad>
2017-06-29 14:14:12 +0800

    落地配与物流时效

--- application/modules/Api/controllers/DestDeliveryLogi.php
+++ application/modules/Api/controllers/DestDeliveryLogi.php
@@ -1,18 +1,30 @@
 <?php
 /**
- * Created by PhpStorm.
+ * 落地配
  * User: linkai
  * Date: 2017/6/28
  * Time: 9:39
  */
-class DestDeliveryLogiController extends ShopMallController{
+class DestDeliveryLogiController extends Yaf_Controller_Abstract{
+    private $input;
+    public function init(){
+        $this->input = json_decode(file_get_contents("php://input"), true);
+    }
+
+    /**
+     * 落地配
+     */
     public function listAction(){
-        $this->put_data['country'] = '';
-        $this->setLang('zh');
+        if(!isset($this->input['country'])){
+            jsonReturn('','1000');
+        }
+        $this->input['lang'] = isset($this->input['lang'])? $this->input['lang']:(browser_lang()?browser_lang():'en');
         $ddlModel = new DestDeliveryLogiModel();
-        $data = $ddlModel->getList($this->put_data['country'],$this->lang);
-        //if($data){
-            jsonReturn($data);
-       //}
+        $data = $ddlModel->getList( $this->input['country'],$this->input['lang']);
+        if($data){
+            jsonReturn(array('data'=>$data));
+        }else{
+            jsonReturn('','400','失败');
+        }
     }
 }
