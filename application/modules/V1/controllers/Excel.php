8cb0e7ab137b2b3ffbcd29bf753a458ff0828441
maimaiti <742163033@qq.com>
2017-07-05 18:52:05 +0800

    写入权限测试

--- application/modules/V1/controllers/Excel.php
+++ application/modules/V1/controllers/Excel.php
@@ -342,14 +342,14 @@ class ExcelController extends PublicController
      */
     private function export_to_disc($obj, $path, $filename) {
         //保存路径，不存在则创建
-        $savePath = $_SERVER['HTTP_HOST'] . "/application/" . $path . "/";
+/*        $savePath = $_SERVER['HTTP_HOST'] . "/application/" . $path . "/";
         if (!is_dir($savePath)) {
             mkdir($savePath, 0775, true);
-        }
+        }*/
         $obj->save($savePath . $filename);
-	$fullPath = $savePath . $filename;
-        $fullPath = $_SERVER['HTTP_HOST'].'/application/ExcelFiles/'.basename($fullPath);
-        return $fullPath;
+	    //$fullPath = $savePath . $filename;
+        //$fullPath = $_SERVER['HTTP_HOST'].'/application/ExcelFiles/'.basename($fullPath);
+        return $filename;
     }
 
     /**
@@ -613,27 +613,19 @@ class ExcelController extends PublicController
         print_r($goods);*/
         $file = $this->goodsListHandler();
 
-        if ($this->check_remote_file_exists($file))
-        {
-            //文件名
-            $filename =str_replace(dirname($file).'/','',$file);
+        //文件名
+        $filename =str_replace(dirname($file).'/','',$file);
 
-            $response = [
-                'code'=>1,
-                'message'=>ErrorMsg::getMessage('1'),
-                'data'=>[
-                    'file'=>$file,
-                    //文件创建时间(文件创建时是指xls文件被导出的时间)
-                    'exported_at'=>strstr($filename,'_',true)
-                ]
-            ];
-        }else{
-            $response = [
-                'code'=>0,
-                'message'=>ErrorMsg::getMessage('0'),
-                'data'=>[]
-            ];
-        }
+        $response = [
+            'code'=>1,
+            'message'=>ErrorMsg::getMessage('1'),
+            'data'=>[
+                'file'=>$file,
+                //文件创建时间(文件创建时是指xls文件被导出的时间)
+                'exported_at'=>strstr($filename,'_',true)
+            ]
+        ];
+
         exit(json_encode($response));
     }
 
@@ -721,8 +713,10 @@ class ExcelController extends PublicController
 
         //保存文件
         $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+        $objWriter->save("demo.1.xlsx");
+        return "demo.1.xlsx";
         //保存到服务器指定目录
-        return $this->export_to_disc($objWriter, "ExcelFiles", date('YmdHis',time())."_goodsList.xls");
+        //return $this->export_to_disc($objWriter, "ExcelFiles", date('YmdHis',time())."_goodsList.xls");
     }
 
     /**
