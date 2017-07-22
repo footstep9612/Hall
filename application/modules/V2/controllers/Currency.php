69be696d4e5937f130b1aa43946f9f54fc18c5a7
zhongyg <zhongyg@zhongyg-note.kr.ad>
2017-07-22 10:34:48 +0800

    汇率
    Signed-off-by: zhongyg <zhongyg@zhongyg-note.kr.ad>

---
+++ application/modules/V2/controllers/Currency.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * 币种
+ * @author  zhongyg
+ */
+class CurrencyController extends PublicController {
+
+  public function init() {
+    parent::init();
+  }
+
+  /**
+   * 获取所有币种
+   * @param string $lang
+   * @param string $country
+   * @return array|mixed
+   * @author  zhongyg
+   */
+  public function listALLAction() {
+    $key = 'Currency_listall';
+    $data = json_decode(redisGet($key), true);
+    $model = new CurrencyModel();
+    if (!$data) {
+      $arr = $model->getlist();
+      if ($arr) {
+        $data['message'] = MSG::getMessage(MSG::MSG_SUCCESS, 'en');
+        $data['code'] = MSG::MSG_SUCCESS;
+        $data['data'] = $arr;
+        redisSet($key, json_encode($data), 86400);
+        $this->jsonReturn($data);
+      } else {
+        $this->setCode(MSG::MSG_FAILED);
+        $this->jsonReturn();
+      }
+    }
+    $this->jsonReturn($data);
+  }
+
+}
