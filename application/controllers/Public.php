1bcaef60bff1803dffdb0180efb2247e7c701dc2
zhongyg <zhongyg@zhongyg-note.kr.ad>
2017-07-25 14:51:58 +0800

    Public.php
    Signed-off-by: zhongyg <zhongyg@zhongyg-note.kr.ad>

--- application/controllers/Public.php
+++ application/controllers/Public.php
@@ -19,8 +19,7 @@ abstract class PublicController extends Yaf_Controller_Abstract {
 
   public function init() {
     ini_set("display_errors", "On");
-    //error_reporting(E_ERROR | E_STRICT);
-    error_reporting(E_ALL);
+    error_reporting(E_ERROR | E_STRICT);
     $this->put_data = $jsondata = $data = json_decode(file_get_contents("php://input"), true);
     $lang = $this->getPut('lang', 'en');
     $this->setLang($lang);
@@ -87,7 +86,8 @@ abstract class PublicController extends Yaf_Controller_Abstract {
 
   public function __call($method, $args) {
     $data['code'] = -1;
-    $data['message'] = 'Action :There is no method ' . $method;;
+    $data['message'] = 'Action :There is no method ' . $method;
+    ;
     $this->jsonReturn($data);
   }
 
