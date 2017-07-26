1efc29b56e0bd4671e8f831b65b3a6df5d13bd84
maimaiti <742163033@qq.com>
2017-07-26 10:13:25 +0800

    完成了产品线报价控制器代码风格整理

--- application/modules/V2/controllers/Productlinequote.php
+++ application/modules/V2/controllers/Productlinequote.php
@@ -1,14 +1,12 @@
 <?php
 
 /**
- * Class ProductlinequoteContoller
- *
  * @description 产品线报价控制器
+ * @file ProductlinequoteContoller.php
  * @author 买买提
  */
-class ProductlinequoteController  extends PublicController
-//class ProductlinequoteController  extends Yaf_Controller_Abstract
-{
+class ProductlinequoteController  extends PublicController{
+
     /**
      * 产品线报价模型
      * @var
@@ -24,33 +22,29 @@ class ProductlinequoteController  extends PublicController
     /**
      * 构造方法
      */
-    public function init()
-    {
+    public function init(){
         parent::init();
-
         $this->productLineQuoteModel = new ProductLineQuoteModel();
         $this->productLineQuoteItemModel = new ProductLineQuoteItemModel();
     }
+
     /**
      * @desc 产品线报价列表接口
      * @author 买买提
      */
-    public function getListAction()
-    {
+    public function getListAction(){
+
         $data = $this->productLineQuoteModel->getList($this->put_data) ;
 
-        if ($data)
-        {
+        if ($data){
             $this->setCode(MSG::MSG_SUCCESS);
-
             $response = [
                 'code'=> $this->getCode(),
                 'message'=> $this->getMessage(),
                 'totalCount'=> intval($data['totalCount'])
             ];
 
-            if (isset($this->put_data['currentPage']) && $this->put_data['currentPage'] && isset($this->put_data['pageSize']) && $this->put_data['pageSize'])
-            {
+            if (isset($this->put_data['currentPage']) && $this->put_data['currentPage'] && isset($this->put_data['pageSize']) && $this->put_data['pageSize']) {
                 $response['currentPage'] = isset($data['currentPage']) ? intval($data['currentPage']) : 1 ;
                 $response['pageSize'] = isset($data['pageSize']) ? intval($data['pageSize']) : 10 ;
             }
@@ -62,16 +56,14 @@ class ProductlinequoteController  extends PublicController
             $this->setCode(MSG::MSG_FAILED);
             $this->jsonReturn();
         }
-
     }
+
     /**
      * @desc 产品线报价详情页询单信息接口
      * @author 买买提
      */
-    public function getInquiryInfoAction()
-    {
-        if (empty($this->put_data['inquiry_no']))
-        {
+    public function getInquiryInfoAction(){
+        if (empty($this->put_data['inquiry_no'])){
             $this->jsonReturn([
                 'code'=> '-101',
                 'message'=> '缺少参数'
@@ -89,25 +81,23 @@ class ProductlinequoteController  extends PublicController
             'message' => '成功',
             'data'=> $data
         ]);
-
     }
 
     /**
      * @desc 产品线报价详情页->询单信息->删除sku
      * @author 买买提
      */
-    public function deleteInquirySkuAction()
-    {
+    public function deleteInquirySkuAction(){
         //这里的删除就是把状态改为DELETED
         $response = $this->productLineQuoteModel->deleteInquirySku($this->put_data);
         $this->jsonReturn($response);
     }
+
     /**
      * @desc 产品线报价详情页商品信息接口
      * @author 买买提
      */
-    public function getGoodsInfoAction()
-    {
+    public function getGoodsInfoAction(){
         echo "产品线报价详情页商品信息";
     }
 
@@ -115,8 +105,7 @@ class ProductlinequoteController  extends PublicController
      * @desc 产品线报价审核列表接口
      * @author 买买提
      */
-    public function getCheckListAction()
-    {
+    public function getCheckListAction(){
         echo "产品线报价审核列表";
     }
 
@@ -124,15 +113,14 @@ class ProductlinequoteController  extends PublicController
      * @desc 产品线报价审核详情页接口
      * @author 买买提
      */
-    public function getCheckInfoAction()
-    {
+    public function getCheckInfoAction(){
         echo "产品线报价审核详情页";
     }
+
     /**
      * mongo测试
      */
-    public function mongoTestAction()
-    {
+    public function mongoTestAction(){
         $server = "mongodb://localhost:27017";
         $options = ['connect'=>TRUE];
         $mongo = new MongoClient($server,$options);
