c058a4b11fc1db151e75caa465c0c3c8e0a94e0c
maimaiti <742163033@qq.com>
2017-08-05 10:14:02 +0800

    添加了sku模板，开启了error_reporting(E_ALL)

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-error_reporting(E_ERROR);
+error_reporting(E_ALL);
 header('Access-Control-Allow-Origin:*');
 header('Access-Control-Allow-Headers:x-requested-with,content-type');
 header('Access-Control-Allow-Methods:GET,POST,PUT,DELETE,OPTIONS;');
