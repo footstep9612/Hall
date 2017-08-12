573f801b4b5febf1663af9d0739d11d400dc5971
link <linkai@kr.ad>
2017-08-12 18:13:32 +0800

    a

--- application/V2/controllers/Goods.php
+++ application/V2/controllers/Goods.php
@@ -15,6 +15,7 @@ class GoodsController extends PublicController {
        // error_reporting(E_ERROR);
      parent::init();
         $this->put_data = $this->put_data ? $this->put_data : json_decode(file_get_contents("php://input"), true);
+        Log::write(json_encode($this->put_data),Log::INFO);
     }
 
     /**
@@ -187,7 +188,6 @@ class GoodsController extends PublicController {
           ];
         /*return $this->put_data; */
         $goodsModel = new GoodsModel();
-        Log::write(json_encode($this->put_data),Log::INFO);
         $result = $goodsModel->editSku($this->put_data);
         $this->returnInfo($result);
     }
