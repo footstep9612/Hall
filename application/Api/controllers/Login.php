301c3965884cd1956283f655ba170eae5453dda7
jhw <377812705@qq.com>
2017-08-14 15:19:24 +0800

    修改

--- application/Api/controllers/Login.php
+++ application/Api/controllers/Login.php
@@ -290,7 +290,7 @@ class LoginController extends Yaf_Controller_Abstract {
             $config_shop = $config_obj->shop->toArray();
             $email_arr['url'] = $config_shop['url'];
             $email_arr['key'] = $data_key['key'];
-            $email_arr['first_name'] = $data['first_name'];
+            $email_arr['first_name'] = $check[0]['first_name'];
             $body = $this->getView()->render('login/forgetemail.html',$email_arr);
             send_Mail($data_key['email'],'Password retrieval on ERUI platform',$body,$data['first_name']);
             jsonReturn($data_key,1,'发送成功');
