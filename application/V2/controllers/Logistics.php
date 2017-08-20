f5ee9e0d766e5888a0dbb59691f02467142f8418
liujf <git@liujf>
2017-08-20 20:49:42 +0800

    *

--- application/V2/controllers/Logistics.php
+++ application/V2/controllers/Logistics.php
@@ -230,6 +230,7 @@ class LogisticsController extends PublicController {
 	        $this->quoteItemModel->startTrans();
 	        foreach ($quoteItemList as $quoteItem) {
 	            $quoteUnitPrice = round($data['total_quote_price'] * $quoteItem['exw_unit_price'] / $data['total_exw_price'], 4);
+	            $quoteUnitPrice = $quoteUnitPrice > 0 ? $quoteUnitPrice : 0;
 	            $tmpRes = $this->quoteItemModel->updateItem(['id' => $quoteItem['id']], ['quote_unit_price' => $quoteUnitPrice]);
 	            
 	            if (!$tmpRes) {
