291fd1ed96e74cf00476ecda03eeef901e36b0c3
zhongyg <zhongyg@zhongyg-note.kr.ad>
2017-08-21 15:01:06 +0800

    ES
    Signed-off-by: zhongyg <zhongyg@zhongyg-note.kr.ad>

--- application/V2/models/EsProduct.php
+++ application/V2/models/EsProduct.php
@@ -244,13 +244,12 @@ class EsProductModel extends Model {
             $onshelf_flag = $condition['onshelf_flag'] == 'N' ? 'N' : 'Y';
 
             if ($onshelf_flag === 'N') {
-                $body['query']['bool']['must'][] = ['bool' => [ESClient::SHOULD => [
-                            [ESClient::WILDCARD => ['show_cats.all' => '*"onshelf_flag":"N"*']],
-                            [ESClient::TERM => ['show_cats.all' => '[]']],
-                ]]];
+                $body['query']['bool']['must'][] = [ESClient::TERM => ['onshelf_flag' => 'N']];
             } else {
-                $body['query']['bool']['must'][] = [ESClient::WILDCARD => ['show_cats.all' => '*"onshelf_flag":"Y"*']];
+                $body['query']['bool']['must'][] = [ESClient::TERM => ['onshelf_flag' => 'Y']];
             }
+        } else {
+            $body['query']['bool']['must'][] = [ESClient::TERM => ['onshelf_flag' => 'Y']];
         }
 
 
