062d168ac1d3c65f044dd576853534ea68ed1897
link <linkai@kr.ad>
2017-08-29 14:04:21 +0800

    报审 属性

--- application/V2/models/Goods.php
+++ application/V2/models/Goods.php
@@ -687,14 +687,14 @@ class GoodsModel extends PublicModel {
                 return false;
             }
 
-            $gattr = new GoodsAttrModel();
+          /*  $gattr = new GoodsAttrModel();
             $resAttr = $gattr->modifyAttr($input['sku'], $status);        //属性状态
 
             if (!$resAttr || $resAttr['code'] != 1) {
                 $this->rollback();
 
                 return false;
-            }
+            }*/
 
             $gattach = new GoodsAttachModel();
             $resAttach = $gattach->modifyAttach($input['sku'], $status);  //附件状态
