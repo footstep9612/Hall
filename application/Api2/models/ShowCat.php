33af51a592ad3e6934e0818c7c3ffc5cb94bf615
zhongyg <git@zhongyg>
2017-09-07 14:20:20 +0800

    1.1、按用户IP所在国家 获取 国家下对应的展示分类及其下的商品； 1.2、如果该国家下没有展示分类，统一展示中国 下的展示分类及其下的商品 Signed-off-by: zhongyg <git@zhongyg>

---
+++ application/Api2/models/ShowCat.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * 展示分类
+ * User: linkai
+ * Date: 2017/6/15
+ * Time: 15:52
+ */
+class ShowCatModel extends PublicModel {
+
+    //状态
+    const STATUS_DRAFT = 'DRAFT'; //草稿
+    const STATUS_APPROVING = 'APPROVING'; //审核；
+    const STATUS_VALID = 'VALID'; //生效；
+    const STATUS_DELETED = 'DELETED'; //DELETED-删除
+
+    protected $dbName = 'erui2_goods';
+    protected $tableName = 'show_cat';
+
+    public function __construct() {
+
+        parent::__construct();
+    }
+
+}
