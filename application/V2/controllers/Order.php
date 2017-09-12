8df387e081e2eb674f3f0c557be1c12191adb9c3
jhw <377812705@qq.com>
2017-09-12 15:30:03 +0800

    添加订单

---
+++ application/V2/controllers/Order.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Description of CityController
+ * @author  jhw
+ * @date    2017-9-12 15:29:09
+ * @version V2.0
+ * @desc   订单
+ */
+class OrderController extends PublicController {
+
+    public function init() {
+        //parent::init();
+    }
+
+    
+
+}
