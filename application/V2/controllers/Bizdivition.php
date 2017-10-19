a940e8b60ef9622ecd93266fbf2ab0feb9bb4241
maimaiti <742163033@qq.com>
2017-10-19 20:33:55 +0800

    更改了退回分单员接口

--- application/V2/controllers/Bizdivition.php
+++ application/V2/controllers/Bizdivition.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * 事业部分单员相关接口
+ * @desc   BizdivitionController
+ * @Author 买买提
+ */
 class BizdivitionController extends PublicController{
 
     //请求参数
@@ -15,6 +20,8 @@ class BizdivitionController extends PublicController{
 
     public function rejectToMarket(){
 
+        //$request = $this->validateRequests('inquiry_id');
+
     }
 
     public function rejectToErui(){
@@ -22,6 +29,28 @@ class BizdivitionController extends PublicController{
     }
 
     public function assignQuoter(){
+
+    }
+
+    /**
+     * 验证指定参数是否存在
+     * @param string $params 初始的请求字段
+     * @return array 验证后的请求字段
+     */
+    private function validateRequests($params=''){
+
+        $request = $this->requestParams;
+        unset($request['token']);
+
+        //判断筛选字段为空的情况
+        if ($params){
+            $params = explode(',',$params);
+            foreach ($params as $param){
+                if (empty($request[$param])) $this->jsonReturn(['code'=>'-104','message'=>'缺少参数']);
+            }
+        }
+
+        return $request;
 
     }
 
