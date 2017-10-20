8c5515b35805d8708c262bd25e0bfc50afa504e0
maimaiti <742163033@qq.com>
2017-10-20 11:40:07 +0800

    事业部分单员->退回市场,分配报价人接口

--- application/V2/controllers/Bizdivition.php
+++ application/V2/controllers/Bizdivition.php
@@ -18,17 +18,48 @@ class BizdivitionController extends PublicController{
 
     }
 
-    public function rejectToMarket(){
 
-        //$request = $this->validateRequests('inquiry_id');
+    /**
+     * 退回市场
+     */
+    public function rejectToMarketAction(){
+
+        $request = $this->validateRequests('inquiry_id');
+
+        $inquiry = new InquiryModel();
+        $response = $inquiry->where(['id'=>$request['inquiry_id']])->save([
+            'status'=>'DRAFT',
+            'updated_by' => $this->user['id'],
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+
+        $this->jsonReturn($response);
 
     }
 
-    public function rejectToErui(){
+    /**
+     * 退回易瑞
+     */
+    public function rejectToEruiAction(){
 
     }
 
-    public function assignQuoter(){
+    /**
+     * 分配报价人
+     */
+    public function assignQuoterAction(){
+
+        $request = $this->validateRequests('inquiry_id,quote_id');
+
+        $inquiry = new InquiryModel();
+        $response = $inquiry->where(['id'=>$request['inquiry_id']])->save([
+            'status'=>'BIZ_QUOTING', //事业部报价
+            'quote_id' => $request['quote_id'],
+            'updated_by' => $this->user['id'],
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+
+        $this->jsonReturn($response);
 
     }
 
