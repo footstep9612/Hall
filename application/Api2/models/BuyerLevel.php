a12e84fcf31e9ee9977a3be84535d3182b81a2e7
konglp <konglp@qq.com>
2017-10-25 15:00:49 +0800

    Api2/controllers/Membercenter.php   增加会员等级列表接口,匹配四种语言
    Signed-off-by: konglp <konglp@qq.com>

--- application/Api2/models/BuyerLevel.php
+++ application/Api2/models/BuyerLevel.php
@@ -9,6 +9,10 @@ class BuyerLevelModel extends PublicModel{
     protected $dbName = 'erui2_config';
     protected $tableName = 'buyer_level';
 
+    const STATUS_VALID = 'VALID';
+    const DELETE_Y = 'Y';
+    const DELETE_N = 'N';
+
     public function __construct(){
         parent::__construct();
     }
@@ -138,5 +142,30 @@ class BuyerLevelModel extends PublicModel{
         } else {
             return false;
         }
+    }
+
+
+    /**
+     * 获取列表
+     * @param mix $condition
+     * @return mix
+     */
+    public function getlist($order=" id desc") {
+        $where = [
+            'status'=> self::STATUS_VALID,
+            'deleted_flag'=> self::DELETE_N
+        ];
+         $result = $this->where($where)->order($order)->select();
+        $buyerLevel = $list = [];
+        if($result) {
+            foreach ($result as $item) {
+                $item['buyer_level'] = json_decode($item['buyer_level'], true);
+                foreach($item['buyer_level'] as $value) {
+                    $buyerLevel['buyer_level'][$value['lang']] = $value;
+                }
+                $list[] = $buyerLevel['buyer_level'];
+            }
+        }
+        return $list;
     }
 }
