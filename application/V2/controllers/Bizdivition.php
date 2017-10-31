c187a2f2b306679bf724d5535d8925bbc92460df
maimaiti <742163033@qq.com>
2017-10-31 18:20:53 +0800

    接口代码规范化

--- application/V2/controllers/Bizdivition.php
+++ application/V2/controllers/Bizdivition.php
@@ -16,7 +16,7 @@ class BizdivitionController extends PublicController{
 
         parent::init();
 
-        $this->inquiryModel = new InquiryModel();
+        $this->inquiryModel  = new InquiryModel();
         $this->requestParams = json_decode(file_get_contents("php://input"), true);
 
     }
@@ -32,10 +32,10 @@ class BizdivitionController extends PublicController{
         $inquiry = new InquiryModel();
         $now_agent_id = $inquiry->where(['id'=>$request['inquiry_id']])->getField('updated_by');
         $response = $inquiry->where(['id'=>$request['inquiry_id']])->save([
-            'status'=>'DRAFT',
-            'now_agent_id'=>$now_agent_id,
-            'updated_by' => $this->user['id'],
-            'updated_at' => date('Y-m-d H:i:s')
+            'status'       => 'DRAFT',
+            'now_agent_id' => $now_agent_id,
+            'updated_by'   => $this->user['id'],
+            'updated_at'   => date('Y-m-d H:i:s')
         ]);
 
         $this->jsonReturn($response);
@@ -56,11 +56,11 @@ class BizdivitionController extends PublicController{
         $roleUser = $roleUserModel->where(['role_id'=>$role_id])->getField('employee_id');
 
         $response = $inquiry->where(['id'=>$request['inquiry_id']])->save([
-            'status' => 'CC_DISPATCHING', //易瑞客户中心
-            'erui_id' => $roleUser,
+            'status'       => 'CC_DISPATCHING', //易瑞客户中心
+            'erui_id'      => $roleUser,
             'now_agent_id' => $roleUser,
-            'updated_by' => $this->user['id'],
-            'updated_at' => date('Y-m-d H:i:s')
+            'updated_by'   => $this->user['id'],
+            'updated_at'   => date('Y-m-d H:i:s')
         ]);
 
         $this->jsonReturn($response);
@@ -77,12 +77,12 @@ class BizdivitionController extends PublicController{
         $inquiry = new InquiryModel();
 
         $response = $inquiry->where(['id'=>$request['inquiry_id']])->save([
-            'status'=>'BIZ_QUOTING', //事业部报价
-            'quote_status'=>'ONGOING', //报价中
-            'quote_id' => $request['quote_id'],
+            'status'       => 'BIZ_QUOTING', //事业部报价
+            'quote_status' => 'ONGOING', //报价中
+            'quote_id'     => $request['quote_id'],
             'now_agent_id' => $request['quote_id'],
-            'updated_by' => $this->user['id'],
-            'updated_at' => date('Y-m-d H:i:s')
+            'updated_by'   => $this->user['id'],
+            'updated_at'   => date('Y-m-d H:i:s')
         ]);
 
         $quoteModel = new QuoteModel();
@@ -92,24 +92,24 @@ class BizdivitionController extends PublicController{
         if (!$flag){
             $inquiryInfo = $inquiry->where(['id'=>$request['inquiry_id']])->find();
             $quote_id = $quoteModel->add($quoteModel->create([
-                'inquiry_id' => $request['inquiry_id'],
-                'serial_no' => $request['serial_no'],
-                'quote_no' => $this->getQuoteNo(),
-                'created_by' => $this->user['id'],
-                'created_at' => date('Y-m-d H:i:s'),
-                'status' => 'BIZ_QUOTING',
+                'inquiry_id'     => $request['inquiry_id'],
+                'serial_no'      => $request['serial_no'],
+                'quote_no'       => $this->getQuoteNo(),
+                'created_by'     => $this->user['id'],
+                'created_at'     => date('Y-m-d H:i:s'),
+                'status'         => 'BIZ_QUOTING',
                 'trade_terms_bn' => $inquiryInfo['trade_terms_bn'],
-                'payment_mode' => $inquiryInfo['payment_mode'],
-                'trans_mode_bn' => $inquiryInfo['trans_mode_bn'],
-                'quote_cur_bn' => $inquiryInfo['cur_bn'],
-                'from_country' => $inquiryInfo['from_country'],
-                'from_port' => $inquiryInfo['from_port'],
+                'payment_mode'   => $inquiryInfo['payment_mode'],
+                'trans_mode_bn'  => $inquiryInfo['trans_mode_bn'],
+                'quote_cur_bn'   => $inquiryInfo['cur_bn'],
+                'from_country'   => $inquiryInfo['from_country'],
+                'from_port'      => $inquiryInfo['from_port'],
                 'dispatch_place' => $inquiryInfo['dispatch_place'],
-                'to_country' => $inquiryInfo['to_country'],
-                'to_port' => $inquiryInfo['to_port'],
-                'delivery_addr' => $inquiryInfo['delivery_addr'],
+                'to_country'     => $inquiryInfo['to_country'],
+                'to_port'        => $inquiryInfo['to_port'],
+                'delivery_addr'  => $inquiryInfo['delivery_addr'],
                 'payment_period' => $inquiryInfo['payment_period'],
-                'delivery_addr' => $inquiryInfo['destination']
+                'delivery_addr'  => $inquiryInfo['destination']
             ]));
 
             $inquiryItemModel = new InquiryItemModel();
@@ -118,14 +118,14 @@ class BizdivitionController extends PublicController{
             $quoteItemModel = new QuoteItemModel();
             foreach ($inquiryItems as $item=>$value){
                 $quoteItemModel->add($quoteItemModel->create([
-                    'quote_id' => $quote_id,
-                    'inquiry_id' => $request['inquiry_id'],
+                    'quote_id'        => $quote_id,
+                    'inquiry_id'      => $request['inquiry_id'],
                     'inquiry_item_id' => $value['id'],
-                    'sku' => $value['sku'],
-                    'quote_qty' => $value['qty'],
-                    'quote_unit' => $value['unit'],
-                    'created_by' => $this->user['id'],
-                    'created_at' => date('Y-m-d H:i:s')
+                    'sku'             => $value['sku'],
+                    'quote_qty'       => $value['qty'],
+                    'quote_unit'      => $value['unit'],
+                    'created_by'      => $this->user['id'],
+                    'created_at'      => date('Y-m-d H:i:s')
                 ]));
             }
         }else{
