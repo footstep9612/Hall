c36bfad6311c1f2ca462a4d630f2f332e6cd6fcd
zhongyg <zhongyg@keruigroup.com>
2017-11-02 09:50:29 +0800

    ES Signed-off-by: zhongyg <zhongyg@keruigroup.com>

--- application/V2/models/EsProduct.php
+++ application/V2/models/EsProduct.php
@@ -937,7 +937,7 @@ class EsProductModel extends Model {
         $body['supplier_count'] = strval($body['supplier_count']);
         $this->_findnulltoempty($body);
         if ($es_product) {
-            $es->add_document($this->dbName, $this->tableName . '_' . $lang, $id);
+            $es->add_document($this->dbName, $this->tableName . '_' . $lang, $body, $id);
         } else {
             $flag = $es->update_document($this->dbName, $this->tableName . '_' . $lang, $body, $id);
         } if (!isset($flag['_version'])) {
