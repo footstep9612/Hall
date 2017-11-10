7c8f6cc11c2f631c6a73e1668466d953dbdd99f4
zhongyg <zhongyg@keruigroup.com>
2017-11-10 16:01:01 +0800

    供应链 Signed-off-by: zhongyg <zhongyg@keruigroup.com>

---
+++ application/V2/models/SupplierChain.php
@@ -0,0 +1,246 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of SupplierChain
+ * @author  zhongyg
+ * @date    2017-11-10 13:42:02
+ * @version V2.0
+ * @desc
+ */
+class SupplierChainModel extends PublicModel {
+
+    //put your code here
+    protected $tableName = 'supplier';
+    protected $dbName = 'erui_supplier';
+
+    const STATUS_VALID = 'APPROVED'; //有效,通过
+    const STATUS_INVALID = 'INVALID'; //无效；
+    const STATUS_TEST = 'TEST'; //待报审；
+    const STATUS_CHECKING = 'STATUS_CHECKING'; //审核；
+    const STATUS_DELETED = 'DELETED'; //删除；
+    const ERUI_STATUS_VALID = 'VALID';          //有效
+    const ERUI_STATUS_CHECKING = 'CHECKING';          //审核中
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    /**
+     * 根据条件获取查询条件
+     * @param mix $condition 查询条件
+     * @param mix $where 返回条件
+     * @return mix
+     * @author zyg
+     *
+     */
+    protected function _getcondition($condition = [], &$where = []) {
+
+        $where = ['deleted_flag' => 'N',
+            'status' => ['in', ['APPROVED', 'VALID']]
+        ];
+        $this->_getValue($where, $condition, 'supplier_no');
+        $this->_getValue($where, $condition, 'supplier_name', 'like', 'name');
+        $this->_getValue($where, $condition, 'erui_status');
+        $this->_getValue($where, $condition, 'is_erui', 'bool');
+        $this->_getValue($where, $condition, 'erui_checked_at', 'between');
+        $this->_getValue($where, $condition, 'created_at', 'between');
+        if (!empty($condition['erui_checked_name'])) {
+            $employee_model = new EmployeeModel();
+            $userids = $employee_model->getUseridsByUserName(trim($condition['created_by_name']));
+            if ($userids) {
+                $where['erui_checked_by'] = ['in', $userids];
+            } else {
+                $where['erui_checked_by'] = '-1';
+            }
+        }
+    }
+
+    /**
+     * 获取列表
+     * @param mix $condition
+     * @return mix
+     * @author zyg
+     */
+    public function getList($condition = [], $order = " id desc") {
+        $where = [];
+        $this->_getcondition($condition, $where);
+        list($offset, $size) = $this->_getPage($condition);
+        $data = $this->field('id,supplier_no,serial_no,name,erui_status,erui_checked_at,erui_checked_by,supplier_level,'
+                        . 'org_id,is_erui')
+                ->limit($offset, $size)
+                ->where($where)
+                ->order($order)
+                ->select();
+        $this->_setEruiStatus($data);
+        $this->_setEruiCheckedName($data);
+        $this->_setOrgName($data);
+        return $data;
+    }
+
+    /**
+     * 获取列表
+     * @param mix $condition
+     * @return mix
+     * @author zyg
+     */
+    public function getCount($condition = []) {
+        $where = [];
+        $this->_getcondition($condition, $where);
+        $count = $this->field('id,supplier_no,serial_no,name,erui_status,erui_checked_at,erui_checked_by,supplier_level,'
+                        . 'org_id,is_erui')
+                ->where($where)
+                ->count();
+        return $count;
+    }
+
+    /**
+     * 获取处理状态
+     * @param mix $data
+     * @return
+     * @author zyg
+     */
+    private function _setEruiStatus(&$data) {
+        if ($data) {
+            foreach ($data as $key => $item) {
+
+                switch ($item['erui_status']) {
+                    case self::ERUI_STATUS_CHECKING:
+                        $item['erui_status'] = '待审核';
+                        break;
+                    case self::ERUI_STATUS_VALID:
+                        $item['erui_status'] = '已通过';
+                        break;
+                    default :
+                        $item['erui_status'] = '';
+                        break;
+                }
+                if (empty($item['erui_checked_at'])) {
+                    $item['erui_checked_at'] = '';
+                }
+                if (empty($item['org_id'])) {
+                    $item['org_id'] = '';
+                }
+                if (empty($item['supplier_level'])) {
+                    $item['supplier_level'] = '';
+                }
+                if (empty($item['erui_checked_by'])) {
+                    $item['erui_checked_by'] = '';
+                }
+                if (empty($item['serial_no'])) {
+                    $item['serial_no'] = '';
+                }
+                if (empty($item['supplier_no'])) {
+                    $item['supplier_no'] = '';
+                }
+                $data[$key] = $item;
+            }
+        }
+    }
+
+    /**
+     * 获取处理状态
+     * @param mix $data
+     * @return
+     * @author zyg
+     */
+    private function _setEruiCheckedName(&$data) {
+        if ($data) {
+            $erui_checked_bys = [];
+            foreach ($data as $item) {
+                if ($item['erui_checked_by']) {
+                    $erui_checked_bys[] = $item['erui_checked_by'];
+                }
+            }
+            if ($erui_checked_bys) {
+                $employee_model = new EmployeeModel();
+                $usernames = $employee_model->getUserNamesByUserids($erui_checked_bys);
+            }
+            foreach ($data as $key => $val) {
+                if ($val['erui_checked_by'] && isset($usernames[$val['erui_checked_by']])) {
+                    $val['erui_checked_name'] = $usernames[$val['erui_checked_by']];
+                } else {
+                    $val['erui_checked_name'] = '';
+                }
+
+                $data[$key] = $val;
+            }
+        }
+    }
+
+    /**
+     * 获取处理状态
+     * @param mix $data
+     * @return
+     * @author zyg
+     */
+    private function _setOrgName(&$data) {
+        if ($data) {
+            $org_ids = [];
+            foreach ($data as $item) {
+                if ($item['org_id']) {
+                    $org_ids[] = $item['org_id'];
+                }
+            }
+            if ($org_ids) {
+                $org_model = new OrgModel();
+                $orgs = $org_model->field('id,name')->where(['id' => $org_ids,
+                            'deleted_flag' => 'N', 'status' => 'NORMAL'])->select();
+                $orgnames = [];
+                if ($orgs) {
+                    foreach ($orgs as $org) {
+                        $orgnames[$org['id']] = $org['name'];
+                    }
+                }
+            }
+
+            foreach ($data as $key => $val) {
+                if ($val['org_id'] && isset($orgnames[$val['org_id']])) {
+                    $val['org_name'] = $orgnames[$val['org_id']];
+                } else {
+                    $val['org_name'] = '';
+                }
+
+                $data[$key] = $val;
+            }
+        }
+    }
+
+    /**
+     * 批量更新供应商等级
+     * @param mix $supplier_ids 供应商ID 数组
+     * @param int $supplier_level 供应商等级
+     * @return
+     * @author zyg
+     */
+    public function batchUpdateLevel($supplier_ids, $supplier_level) {
+
+        $where = ['deleted_flag' => 'N', 'id' => $supplier_ids];
+        $data['supplier_level'] = $supplier_level;
+        return $this->where($where)->save($data);
+    }
+
+    /**
+     * 批量更新供应商等级
+     * @param int $supplier_id 供应商ID 数组
+     * @param int $supplier_level 供应商等级
+     * @return
+     * @author zyg
+     */
+    public function Checked($supplier_id, $supplier_level, $is_erui = 'N') {
+
+        $where = ['deleted_flag' => 'N', 'id' => $supplier_id];
+        $data['supplier_level'] = $supplier_level;
+        $data['is_erui'] = $is_erui === 'Y' ? 'Y' : 'N';
+        $data['erui_status'] = self::ERUI_STATUS_VALID;
+        $data['erui_checked_at'] = date('Y-m-d H:i:s');
+        $data['erui_checked_by'] = defined('UID') ? UID : 0;
+        return $this->where($where)->save($data);
+    }
+
+}
