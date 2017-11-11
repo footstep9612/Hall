b47cea16044ef58775d8dfd6d64a1741a5e77474
zhongyg <zhongyg@keruigroup.com>
2017-11-11 22:48:16 +0800

    gys bUG
    Signed-off-by: zhongyg <zhongyg@keruigroup.com>

--- application/V2/models/GoodsCostPrice.php
+++ application/V2/models/GoodsCostPrice.php
@@ -25,7 +25,7 @@ class GoodsCostPriceModel extends PublicModel {
 
     /**
      * 通过商品sku编码获取价格策略信息
-     * @param array $sku
+     * @param array $input
      * @return array
      * @author klp
      */
@@ -36,7 +36,7 @@ class GoodsCostPriceModel extends PublicModel {
         }
         $fields = 'id, sku, supplier_id, price,max_price, price_unit, price_cur_bn, min_purchase_qty,max_purchase_qty, pricing_date, price_validity, status, created_by, created_at';
         try {
-            $result = $this->field($fields)->where(['sku' => $input['sku']])->select();
+            $result = $this->field($fields)->where(['sku' => $input['sku'], 'deleted_flag' => 'N', 'status' => 'VALID'])->select();
             $data = array();
             if ($result) {
                 //通过supplier_id查询供应商名称
@@ -67,6 +67,7 @@ class GoodsCostPriceModel extends PublicModel {
         }
         $results = array();
         try {
+            $this->where(['sku' => $sku])->save(['deleted_flag' => 'Y', 'status' => 'DELETED']);
             foreach ($input as $key => $value) {
                 $data = $this->checkParam($value, $sku);
                 //存在sku编辑,反之新增,后续扩展性
@@ -122,14 +123,14 @@ class GoodsCostPriceModel extends PublicModel {
             $results['code'] = '-1001';
             $results['message'] = '[sku]缺失!';
         }
-       /* if (empty($checkout['min_purchase_qty'])) {
-            $results['code'] = '-1001';
-            $results['message'] = '[最小购买量]缺失!';
-        }
-        if (empty($checkout['price']) || !is_numeric($checkout['price'])) {
-            $results['code'] = '-1001';
-            $results['message'] = '[价格]缺失或非数字!';
-        }*/
+        /* if (empty($checkout['min_purchase_qty'])) {
+          $results['code'] = '-1001';
+          $results['message'] = '[最小购买量]缺失!';
+          }
+          if (empty($checkout['price']) || !is_numeric($checkout['price'])) {
+          $results['code'] = '-1001';
+          $results['message'] = '[价格]缺失或非数字!';
+          } */
 
         if (empty($checkout['supplier_id'])) {
             $results['code'] = '-1001';
@@ -173,41 +174,41 @@ class GoodsCostPriceModel extends PublicModel {
             $data['id'] = $checkout['id'];
         }
 
-        if($results){
+        if ($results) {
             jsonReturn($results);
         }
         return $data;
     }
 
-    protected function _checkCostPrice($data,$field='min_purchase_qty,max_purchase_qty'){
+    protected function _checkCostPrice($data, $field = 'min_purchase_qty,max_purchase_qty') {
         if (isset($data['price']) && isset($data['max_price'])) {
             if ($data['price'] >= $data['max_price']) {
-                jsonReturn('',-1006,'价格区间错误!');
+                jsonReturn('', -1006, '价格区间错误!');
             }
         }
 
         $where = array(
             'supplier_id' => $data['supplier_id'],
-            'deleted_flag'=> 'N'
+            'deleted_flag' => 'N'
         );
         $result = $this->field($field)->where($where)->select();
         if ($result) {
             foreach ($result as $item) {
                 if (!empty($item['min_purchase_qty']) && !empty($item['max_purchase_qty'])) {
-                    $arrNumO = range($item['min_purchase_qty'],$item['max_purchase_qty']);
+                    $arrNumO = range($item['min_purchase_qty'], $item['max_purchase_qty']);
                 } else {
                     $numO = $item['min_purchase_qty'];
                 }
 
                 if (!empty($data['min_purchase_qty']) && !empty($data['max_purchase_qty'])) {
-                    $arrNumT = range($data['min_purchase_qty'],$data['max_purchase_qty']);
+                    $arrNumT = range($data['min_purchase_qty'], $data['max_purchase_qty']);
                 } else {
                     $numT = $data['min_purchase_qty'];
                 }
 
                 if ($arrNumO) {
-                    if($arrNumT) {
-                        $res = array_diff($arrNumO,$arrNumT);
+                    if ($arrNumT) {
+                        $res = array_diff($arrNumO, $arrNumT);
                         if (!empty($res)) {
                             $code = -1006;
                         }
@@ -217,14 +218,14 @@ class GoodsCostPriceModel extends PublicModel {
                         }
                     }
                 } else {
-                    if($arrNumT) {
-                        if (in_array($numO,$arrNumT)) {
+                    if ($arrNumT) {
+                        if (in_array($numO, $arrNumT)) {
                             $code = -1006;
                         }
                     }
                 }
                 if ($code) {
-                    jsonReturn('',$code,'数量区间错误或冲突!');
+                    jsonReturn('', $code, '数量区间错误或冲突!');
                 }
             }
         }
