5d8a34096b1e97fd91e2547534fbfe238e103090
zhongyg <zhongyg@keruigroup.com>
2017-11-24 09:37:10 +0800

    ProductSupplier.php Signed-off-by: zhongyg <zhongyg@keruigroup.com>

--- application/V2/models/ProductSupplier.php
+++ application/V2/models/ProductSupplier.php
@@ -69,9 +69,7 @@ class ProductSupplierModel extends PublicModel {
         $where = [];
         $this->_getCondition($where, $condition);
         $product_model = new ProductModel();
-        //$supplier_model = new SupplierModel();
         $product_table = $product_model->getTableName();
-        // $supplier_table = $supplier_model->getTableName();
         list($starrow, $pagesize) = $this->_getPage($condition);
         return $product_model->alias('pzh')
                         ->field('pzh.name as product_name_zh'
@@ -81,7 +79,6 @@ class ProductSupplierModel extends PublicModel {
                                 . ' and pen.deleted_flag=\'N\''
                                 . ' and  pen.`status` in(\'NORMAL\', \'VALID\', \'TEST\', \'CHECKING\', \'DRAFT\', \'INVALID\')'
                                 , 'left')
-                        // ->join($supplier_table . ' as s on s.id=ps.supplier_id', 'left')
                         ->limit($starrow, $pagesize)
                         ->order('pzh.view_count desc')
                         ->select();
@@ -100,9 +97,7 @@ class ProductSupplierModel extends PublicModel {
         $where = [];
         $this->_getCondition($where, $condition);
         $product_model = new ProductModel();
-        // $supplier_model = new SupplierModel();
         $product_table = $product_model->getTableName();
-        //   $supplier_table = $supplier_model->getTableName();
         $count = $product_model->alias('pzh')
                 ->where($where)
                 ->join($product_table . ' as pen on pen.spu=pzh.spu and pen.lang=\'en\''
@@ -185,15 +180,6 @@ class ProductSupplierModel extends PublicModel {
                     ])
                     ->group('ps.supplier_id,ps.spu')
                     ->select();
-//            $product_attrs = $this->alias('ps')
-//                    ->field('ps.spu,ps.supplier_id,ps.view_count,'
-//                            . '(select name from  erui_supplier.supplier where id=ps.supplier_id ) as supplier_name')
-//                    ->where(['ps.spu' => ['in', $spus],
-//                        'ps.status' => 'VALID',
-//                        'ps.deleted_flag' => 'N'
-//                    ])
-////   ->group('ps.supplier_id,ps.spu')
-//                    ->select();
 
 
             if (!$product_attrs) {
@@ -240,14 +226,7 @@ class ProductSupplierModel extends PublicModel {
                 }
                 $product_supplier = $this->field('id,deleted_flag')->where(['supplier_id' => $data['supplier_id'], 'spu' => $spu])->find();
 
-                /** 感觉这里的brand就是个鸡肋，而且没用 　暂时隐藏掉　 */
-                /* $product_model = new ProductModel();
-                  $product = $product_model->where(['spu' => $spu, 'lang' => 'zh'])->find();
-                  if (empty($product)) {
-                  $product = $product_model->where(['spu' => $spu, 'lang' => 'en'])->find();
-                  }
-                  $data['brand'] = isset($product['brand']) ? $product['brand'] : '{"lang": "zh", "name": "", "logo": "", "manufacturer": ""}';
-                 */
+
                 if ($product_supplier) {
                     if ($product_supplier['deleted_flag'] != 'N') {
                         $data['updated_by'] = $admin;
@@ -506,14 +485,14 @@ class ProductSupplierModel extends PublicModel {
         $goods_model = new GoodsModel();
         $goods_table = $goods_model->getTableName();
         $tmp_table = '(SELECT fqi.inquiry_id,'
-                . 'sum(if(fqi.exw_unit_price+fqi.quote_unit_price>0,(fqi.exw_unit_price+fqi.quote_unit_price)*qi.quote_qty,0)) as total_quote_price,'
+                . 'sum(if(fqi.exw_unit_price+fqi.quote_unit_price>0,fqi.exw_unit_price+fqi.quote_unit_price,0)) as total_quote_price,'
                 . '(SELECT g.spu from ' . $goods_table . ' as g where g.sku=fqi.sku and lang=\'zh\' and deleted_flag=\'N\' '
                 . 'GROUP BY g.sku )  as spu '
-                . 'from ' . $final_quote_item_table . ' fqi left join ' . $quote_item_table . ' qi on qi.id=fqi.quote_item_id and qi.deleted_flag=\'N\''
-                . ' where fqi.deleted_flag=\'N\' and fqi.`status`=\'VALID\' GROUP BY fqi.inquiry_id,spu)  tmp_table';
+                . 'from ' . $final_quote_item_table . ' fqi left join ' . $quote_item_table
+                . ' on  qi.id=fqi.quote_item_id and  qi.deleted_flag=\'N\'  where fqi.deleted_flag=\''
+                . 'N\' and fqi.`status`=\'VALID\' GROUP BY fqi.inquiry_id,spu)  tmp_table';
         $inquiryinfo = $this->query('select count(inquiry_id) as quote_num,avg(total_quote_price) as avg_quote_price from '
                 . $tmp_table . ' where spu=\'' . $spu . '\'');
-
 
         $item['avg_price'] = isset($inquiryinfo[0]['avg_quote_price']) ? $inquiryinfo[0]['avg_quote_price'] : 0;
         $item['quote_num'] = $inquiryinfo[0]['quote_num'];
@@ -530,10 +509,10 @@ class ProductSupplierModel extends PublicModel {
         $final_quote_item_table = $final_quote_item_model->getTableName();
         $goods_model = new GoodsModel();
         $goods_table = $goods_model->getTableName();
+        $quote_item_model = new QuoteItemModel();
+        $quote_item_table = $quote_item_model->getTableName();
 
-        $product_model = new ProductModel();
-        $product_table = $product_model->getTableName();
-        $tmp_table = '(SELECT fqi.inquiry_id,sum(if(fqi.total_quote_price>0,fqi.total_quote_price,0)) as total_quote_price,'
+        $tmp_table = '(SELECT fqi.inquiry_id,'
                 . '(SELECT g.spu from ' . $goods_table . ' as g where g.sku=fqi.sku and lang=\'zh\' and deleted_flag=\'N\' '
                 . 'GROUP BY g.sku )  as spu '
                 . 'from ' . $final_quote_item_table . ' fqi where fqi.deleted_flag=\'N\' '
