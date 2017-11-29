c7686f61255f74e746940bc3aadd0483f53f1d26
wangs <footstep6978@163.com>
2017-11-29 19:15:44 +0800

    yewu

---
+++ application/V2/controllers/Buyerbusiness.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+创建客户---业务信息
+ * 王帅
+ */
+class BuyerbusinessController extends PublicController
+{
+
+    public function __init()
+    {
+        parent::__init();
+    }
+    /*
+     * 创建客户---业务信息
+     * */
+    public function createBusinessAction()
+    {
+        $created_by = '39305';
+        $data = json_decode(file_get_contents("php://input"), true);
+        $data['created_by'] = $created_by;
+        $business = new BuyerBusinessModel();
+        $businessRes = $business->createBusiness($data);
+        if(!empty($data['purchase'])){
+            $purchase = new BuyerPurchasingModel();
+            $purchaseRes = $purchase->createPurchase($data['purchase'],$data['created_by']);
+        }
+        if(!$businessRes || !$purchaseRes){
+            echo json_encode(array("code" => "0", "data" => $res, "message" => "创建失败"));
+            exit();
+        }
+        echo json_encode(array("code" => "1", "data" => $res, "message" => "创建成功"));
+    }
+}
--- application/V2/models/BuyerBusiness.php
+++ application/V2/models/BuyerBusiness.php
@@ -4,90 +4,113 @@ class BuyerBusinessModel extends PublicModel
 {
     protected $dbName = 'erui_buyer'; //数据库名称
     protected $tableName = 'buyer_business'; //采购商业务信息表名
+    protected $buyer_id = 123; //采购商id
     public function __construct()
     {
         parent::__construct();
     }
-
-    //客户档案管理搜索列表index,wangs
-    public function createBusiness($data)
-    {
+    //验证有效数据性
+    public function validData($data){
         $validArr = array(
             'product_type', //所需产品类型
             'purchasing_model', //采购模式
-            'purchasing_cycle', //采购周期
-            'usage',    //设备使用情况
-            'is_warehouse', //是否有仓库
-            'warehouse_address',    //仓库所在地
-            'competitor_info',  //客户竞争对手信息
-            'trade_terms',  //客户惯用贸易术语
-            'settlement',   //结算方式
-            'is_local_settlement',  //是否本地决算
-            'is_net',   //是否入网
-            'is_purchasing_relationship',   //采购关系
-            'net_at',   //入网时间
-            'net_invalid_at',   //失效时间
-            'net_goods',    //入网产品+
+//            'purchasing_cycle', //采购周期
+//            'trade_terms',  //客户惯用贸易术语+
+//            'settlement',   //结算方式
+//            'is_local_settlement',  //是否本地决算
+//            'is_purchasing_relationship',   //采购关系
+//            'is_net',   //是否入网
+//            'net_at',   //入网时间
+//            'net_invalid_at',   //失效时间
+//            'net_goods',    //入网产品+
         );
-        //业务基础信息,结算基本情况,入网管理
-        $purchase = $data['product_type'];  //所需产品类型 varchar
-        $purchase = $data['purchasing_model'];  //采购模式 varchar
-        $purchase = $data['purchasing_cycle'];  //采购周期 varchar
-        $purchase = $data['usage'];  //设备使用情况 varchar
-        $purchase = $data['is_warehouse'];  //是否有仓库 char
-        $purchase = $data['warehouse_address'];  //仓库所在地 varchar
-        $purchase = $data['competitor_info'];  //客户竞争对手信息 varchar
-        $purchase = $data['trade_terms'];  //客户惯用贸易术语 varchar
-        $purchase = $data['settlement'];  //结算方式 varchar
-        $purchase = $data['is_local_settlement'];  //是否本地决算 char
-        $purchase = $data['is_net'];  //是否入网 char
-        $purchase = $data['net_subject'];  //入网主体 char
-        $purchase = $data['net_at'];  //入网时间 date
-        $purchase = $data['net_invalid_at'];  //失效时间 date
-        $purchase = $data['net_goods'];  //入网产品 varchar
-        //采购计划
-        $purchase = $data['purchase'];  //采购计划
-
-//        $purchase = $data['purchasing_at'];  //采购年份date
-//        $purchase = $data['purchasing_plan'];  //采购计划varchar
-//        $purchase = $data['purchasing_budget'];  //采购预算varchar
-        //上传附件
-
-
-
-
-//        $page = isset($data['page'])&&!empty($data['page']) ? $data['page'] : 1;
-//        $offset = ($page-1)*2;
-//        $map = array('buyer.created_by'=>$data['created_by']);
-        if(!empty($data['product_type'])){
-            $map += array('buyer.area_bn'=>$data['area_bn']);
+        unset($data['token']);
+        unset($data['purchase']);
+        foreach($data as $key => $value){
+            foreach($validArr as $v){
+                if(empty($data[$v])){
+                    return false;
+                }
+            }
+            if(!in_array($key,$validArr)){
+                if(strlen($value)>200*3){
+                    return false;
+                }
+            }
         }
-//        if(!empty($data['country_bn'])){
-//            $map += array('buyer.country_bn'=>$data['country_bn']);
-//        }
-//        if(!empty($data['buyer_code'])){
-//            $map += array('buyer.buyer_code'=>$data['buyer_code']);
-//        }
-//        if(!empty($data['name'])){
-//            $map += array('buyer.name'=>$data['name']);
-//        }
-//        if(!empty($data['buyer_level'])){
-//            $map += array('buyer.buyer_level'=>$data['buyer_level']);
-//        }
-//        if(!empty($data['reg_capital'])){
-//            $map += array('buyer.reg_capital'=>$data['reg_capital']);
-//        }
-//        if(!empty($data['line_of_credit'])){
-//            $map += array('buyer.line_of_credit'=>$data['line_of_credit']);
-//        }
-//        $info = $this->alias('buyer')
-//            ->join('erui_buyer.buyer_account account on buyer.id=account.buyer_id','left')
-//            ->join('erui_buyer.buyer_business business on account.buyer_id=business.buyer_id','left')
-//            ->field('buyer.id,buyer.buyer_code,buyer.name,buyer.area_bn,buyer.country_bn,buyer.line_of_credit,buyer.credit_available,buyer.buyer_level,buyer.level_at,buyer.credit_level,buyer.reg_capital,buyer.created_by,account.email,business.is_local_settlement,business.is_purchasing_relationship,business.is_net,business.net_at,business.net_invalid_at')
-//            ->where($map)
-//            ->limit($offset,2)
-//            ->select();
-//        return $info;
+        if($data['net_at'] > $data['net_invalid_at'] || $data['net_at'] < date('Y-m-d')){
+            return false;
+        }
+        return true;
     }
-
+    //客户档案管理搜索列表index,wangs
+    public function createBusiness($data)
+    {
+        $buyer_id = 123;
+        $created_by = $data['created_by'];
+        //验证
+        $validRes = $this -> validData($data);
+        if(!$validRes){
+            return false;
+        }
+        //组装数据
+        $arr = array(
+            'buyer_id' => 123,
+            'created_by' => $created_by,
+            'created_at' => date('Y-m-d H:i:s'),
+            'product_type' => $data['product_type'],
+            'purchasing_model' => $data['purchasing_model'],
+            'purchasing_cycle' => $data['purchasing_cycle'],
+            'trade_terms' => $data['trade_terms'],
+            'settlement' => $data['settlement'],
+            'is_local_settlement' => $data['is_local_settlement'],
+            'is_purchasing_relationship' => $data['is_purchasing_relationship'],
+            'is_net' => $data['is_net'],
+            'net_at' => $data['net_at'],
+            'net_invalid_at' => $data['net_invalid_at'],
+            'net_goods' => $data['net_goods']
+        );
+        $arrOpt = array();
+        if(!empty($data['usage'])){
+            $arrOpt['usage'] += $data['usage'];
+        }
+        if(!empty($data['is_warehouse'])){
+            $arrOpt['is_warehouse'] += $data['is_warehouse'];
+        }
+        if(!empty($data['warehouse_address'])){
+            $arrOpt['warehouse_address'] += $data['warehouse_address'];
+        }
+        if(!empty($data['competitor_info'])){
+            $arrOpt['competitor_info'] += $data['competitor_info'];
+        }
+        if(!empty($data['net_subject'])){
+            $arrOpt['net_subject'] += $data['net_subject'];
+        }
+        if(!empty($arrOpt)){
+            $arr = array_merge($arr,$arrOpt);
+        }
+        //数据存在，删除
+        $showRes = $this -> showBusiness($buyer_id,$created_by);
+        if(!empty($showRes)){
+            $map = array(
+                'buyer_id'=>$buyer_id,
+                'created_by'=>$created_by
+            );
+            $this -> where($map) -> delete();
+        }
+        //添加数据
+        $addRes = $this -> add($arr);
+        if($addRes){
+            return true;
+        }
+    }
+    //查询业务信息
+    public function showBusiness($buyer_id,$created_by){
+        $map = array(
+            'buyer_id'=>$buyer_id,
+            'created_by'=>$created_by
+        );
+        $info = $this -> where($map) -> select();
+        return $info;
+    }
 }
