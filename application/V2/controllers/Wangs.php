8fbfc0904e8ccbc9d67c16483aecbb1fbdee479f
wangs <footstep6978@163.com>
2017-11-29 09:04:12 +0800

    w   b

--- application/V2/controllers/Wangs.php
+++ application/V2/controllers/Wangs.php
@@ -17,7 +17,20 @@ class WangsController extends PublicController
     {
         parent::__init();
     }
-    public function testAction(){
-        echo 123;
+    /*
+     * 客户管理列表
+     * */
+    public function buyerListAction()
+    {
+        $created_by = '39305';
+        $data = json_decode(file_get_contents("php://input"), true);
+        $data['created_by'] = $created_by;
+        $model = new BuyerModel();
+        $res = $model->buyerList($data);
+        if(!$res){
+            echo json_encode(array("code" => "0", "data" => $res, "message" => "空数据"));
+            exit();
+        }
+        echo json_encode(array("code" => "1", "data" => $res, "message" => "返回数据"));
     }
 }
