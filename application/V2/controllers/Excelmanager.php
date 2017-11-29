a86e417ab25d3be54841a75046d6695e1ca2d052
zhengkq <zhengkq@keruigroup.com>
2017-11-29 22:03:22 +0800

    询单下载修改配置

--- application/V2/controllers/Excelmanager.php
+++ application/V2/controllers/Excelmanager.php
@@ -343,12 +343,8 @@ class ExcelmanagerController extends PublicController {
         curl_setopt($ch,CURLOPT_COOKIE,$cookies);
         $response = curl_exec($ch);
         if (curl_errno($ch)) {
-            echo $cookies."\n";
-            print_r($data);
-            print_r(curl_error($ch));
-            die();
             return [];
-        }
+        }        
         curl_close($ch);
         $cfile = null;
         unset($cfile);
