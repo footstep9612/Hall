f748d67ba32cbc15c915e0659af53905c396d984
maimaiti <742163033@qq.com>
2017-11-30 17:08:09 +0800

    代办人任务显示列表数据状态调整

--- application/V2/controllers/Notification.php
+++ application/V2/controllers/Notification.php
@@ -20,7 +20,7 @@ class NotificationController extends PublicController
         $pagesize = !empty($request['pageSize']) ? $request['pageSize'] : 10;
 
         $inquiry = new InquiryModel();
-        $list = $inquiry->where(['now_agent_id'=>$this->user['id']])->order('id DESC')->field('id,serial_no,inflow_time,status,quote_status')->page($page, $pagesize)->select();
+        $list = $inquiry->where(['now_agent_id'=>$this->user['id']])->where("status !='INQUIRY_CLOSED' ")->order('id DESC')->field('id,serial_no,inflow_time,status,quote_status')->page($page, $pagesize)->select();
 
         foreach ($list as &$item){
             $item['remind_count'] = count($this->remindList($item['id']));
@@ -31,7 +31,7 @@ class NotificationController extends PublicController
         $this->jsonReturn([
             'code'    => 1,
             'message' => '成功!',
-            'count'   => count($inquiry->where(['now_agent_id'=>$this->user['id']])->order('id DESC')->field('id,serial_no,inflow_time,status,quote_status')->select()),
+            'count'   => count($inquiry->where(['now_agent_id'=>$this->user['id']])->where("status !='INQUIRY_CLOSED' ")->field('id')->select()),
             'data'    => $list
         ]);
 
