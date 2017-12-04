8c18ee4fa0a55f01e18272426219f31c6eb7bf90
wangs <footstep6978@163.com>
2017-12-04 15:12:52 +0800

    localt

--- application/V2/controllers/Buyerfiles.php
+++ application/V2/controllers/Buyerfiles.php
@@ -46,17 +46,4 @@ class BuyerfilesController extends PublicController
         $dataJson['page'] = $arr['page'];
         $this -> jsonReturn($dataJson);
     }
-    //查看
-//    public function showBuyerAction(){
-//        $created_by = '39305';
-//        $data = json_decode(file_get_contents("php://input"), true);
-//        $data['created_by'] = $created_by;
-//        $model = new BuyerModel();
-//        $res = $model->showBuyer($data);
-//        if(!$res){
-//            echo json_encode(array("code" => "0", "data" => $res, "message" => "空数据"));
-//            exit();
-//        }
-//        echo json_encode(array("code" => "1", "data" => $res, "message" => "返回数据"));
-//    }
 }
