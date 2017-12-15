184ad8b706da9b375825494c45bbe6309192d2a3
zhongyg <zhongyg@keruigroup.com>
2017-12-15 21:16:52 +0800

    Home.php
    Signed-off-by: zhongyg <zhongyg@keruigroup.com>

---
+++ application/Mall/models/StockCountryAds.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of 现货国家
+ * @author  zhongyg
+ * @date    2017-12-6 9:12:49
+ * @version V2.0
+ * @desc
+ */
+class StockCountryAdsModel extends PublicModel {
+
+    //put your code here
+    protected $tableName = 'stock_country_ads';
+    protected $dbName = 'erui_stock';
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    private function _getCondition($condition) {
+        $where = ['deleted_flag' => 'N'];
+        $this->_getValue($where, $condition, 'country_bn');
+        $this->_getValue($where, $condition, 'created_at', 'between');
+        $this->_getValue($where, $condition, 'created_by');
+        $this->_getValue($where, $condition, 'img_name', 'like');
+        $this->_getValue($where, $condition, 'group');
+        return $where;
+    }
+
+    /**
+     * Description of 获取现货国家列表
+     * @author  zhongyg
+     * @date    2017-12-6 9:12:49
+     * @version V2.0
+     * @desc  现货国家
+     */
+    public function getList($condition) {
+        $where = $this->_getCondition($condition);
+        return $this->where($where)->select();
+    }
+
+}
