58f85172784921354d012aef02b484c60c5c6599
wangs <footstep6978@163.com>
2017-12-20 16:38:37 +0800

    visipage

--- application/V2/models/BuyerVisit.php
+++ application/V2/models/BuyerVisit.php
@@ -37,9 +37,11 @@ class BuyerVisitModel extends PublicModel {
         }else{
             $current_no = 1;
         }
+        $pageSize = 10;
+        $offset = ($current_no-1)*$pageSize;
         $total = $this->field('id')->where($condition)->count();
         //按条件获取拜访记录数据
-        $result = $this->condGetVisitData($condition,$i=0,$length=10);
+        $result = $this->condGetVisitData($condition,$offset,$pageSize);
         $arr = [
                  'current_no' => $current_no,
                  'pagesize' => $length,
