85865e6e4a0405211032b73e877500e53e547b18
liujf <git@liujf>
2017-12-21 21:48:03 +0800

    针对获取询单某个时间段内的数据接口中区域没有数据的修改

--- application/V2/controllers/Report.php
+++ application/V2/controllers/Report.php
@@ -228,6 +228,8 @@ class ReportController extends PublicController {
             $inquiryModel = new InquiryModel();
             $inquiryCheckLogModel = new InquiryCheckLogModel();
             $inquiryItemModel = new InquiryItemModel();
+            $marketAreaModel = new MarketAreaModel();
+            $marketAreaCountryModel = new MarketAreaCountryModel();
 
             $nowTime = time();
 
@@ -239,7 +241,11 @@ class ReportController extends PublicController {
 
                 $inquiry['gross_profit_rate'] = $inquiry['gross_profit_rate'] / 100;
                 $inquiry['quote_status'] = $inquiryModel->quoteStatus[$inquiry['quote_status']];
-
+                if (empty($inquiry['area_name'])) {
+                    $area = $marketAreaCountryModel->where(['country_bn' => $inquiry['country_bn']])->getField('market_area_bn');
+                    $inquiry['area_name'] = $marketAreaModel->where(['bn' => $area, 'lang' => 'zh', 'deleted_flag' => 'N'])->getField('name');
+                }
+                
                 if ($inquiry['quote_status'] == 'QUOTED' || $inquiry['quote_status'] == 'COMPLETED') {
                     $quoteTime = $inquiryCheckLogModel->where(['inquiry_id' => $inquiry['id'], 'in_node' => 'MARKET_CONFIRMING'])->getField('out_at');
                     $inquiry['quote_time'] = strtotime($quoteTime) - $createdTime;
