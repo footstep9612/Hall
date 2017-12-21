33a2571dcb9fee494f5e5c0544ec27e76b6ffc2f
wangs <footstep6978@163.com>
2017-12-21 16:41:31 +0800

    count oil

--- application/V2/controllers/Buyer.php
+++ application/V2/controllers/Buyer.php
@@ -166,7 +166,7 @@ class BuyerController extends PublicController {
         $dataJson = array(
             'code'=>1,
             'message'=>'返回数据',
-            'count'=>$ststisInfo['totalCount'],
+            'count'=>intval($ststisInfo['totalCount']),
             'currentPage'=>$ststisInfo['currentPage'],
             'data'=>$ststisInfo['info']
         );
