f3d7d8257fbbfcfa7742504bb3ec3d214dd16ef0
wangs <footstep6978@163.com>
2017-12-28 16:43:00 +0800

    财务

--- application/V2/models/Buyer.php
+++ application/V2/models/Buyer.php
@@ -1291,7 +1291,8 @@ class BuyerModel extends PublicModel {
             $this->where(array('id'=>$arr['id']))->save($arr);
             //创建财务报表附件
             if(!empty($data['base_info']['attach_name']) && !empty($data['base_info']['attach_url'])){
-                $this -> createAttchData($data);
+                $attach = new BuyerattachModel();
+                $attach -> createBuyerFinanceTable($data);
             }
             //创建联系人信息
             $model = new BuyercontactModel();
@@ -1304,20 +1305,6 @@ class BuyerModel extends PublicModel {
             Log::write(__CLASS__ . PHP_EOL . __LINE__ . PHP_EOL . '/v2/buyer/createBuyerInfo:' . $e , Log::ERR);
             return false;   //新建客户基本信息失败
         }
-    }
-
-    /**
-     * @param $data
-     * @return创建财务报表附件
-     */
-    public function createAttchData($data){
-        $attach_name = $data['base_info']['attach_name'];
-        $attach_url = $data['base_info']['attach_url'];
-        $buyer_id = $data['base_info']['buyer_id'];
-        $created_by = $data['created_by'];
-        $model = new BuyerattachModel();
-        $financeRes = $model->createBuyerFinanceTable($attach_name,$attach_url,$buyer_id,$created_by);
-        return $financeRes;
     }
 
     /**
