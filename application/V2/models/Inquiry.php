7ca9ed4d7499e272bc57386a93b8f6986c73cb5d
liujf <git@liujf>
2018-01-15 16:57:56 +0800

    新增项目澄清相关接口

--- application/V2/models/Inquiry.php
+++ application/V2/models/Inquiry.php
@@ -28,6 +28,7 @@ class InquiryModel extends PublicModel {
     const buyerCountryAgent = 'B001'; //区域负责人或国家负责人
     public $inquiryStatus = [
         'DRAFT' => '草稿',
+        'CLARIFY' => '项目澄清',
         'REJECT_MARKET' => '驳回市场',
         'BIZ_DISPATCHING' => '事业部分单员',
         'CC_DISPATCHING' => '易瑞客户中心分单员',
