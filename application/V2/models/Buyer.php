1f741f26e1a7c675aba7162fef273500eb0e36c0
wangs <footstep6978@163.com>
2018-01-17 09:23:04 +0800

    buyerl

--- application/V2/models/Buyer.php
+++ application/V2/models/Buyer.php
@@ -1663,6 +1663,7 @@ EOF;
                 $arr[$k]['is_oilgas'] = $v['is_oilgas']=='Y'?'YES':'NO';    //是否油气
             }
             $arr[$k]['buyer_level'] = $v['buyer_level'];    //客户等级
+            
             $arr[$k]['level_at'] = $v['level_at'];  //等级设置时间
             $arr[$k]['reg_capital'] = $v['reg_capital'];    //注册资金
             $arr[$k]['reg_capital_cur'] = $v['reg_capital_cur'];    //货币
