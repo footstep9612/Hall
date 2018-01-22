9a19e7e1c2169a61308e55b2b239c0a1b7abba5a
liujf <git@liujf>
2018-01-22 10:39:23 +0800

    *

--- application/V2/controllers/Logistics.php
+++ application/V2/controllers/Logistics.php
@@ -586,7 +586,7 @@ class LogisticsController extends PublicController {
 	         
 	        $res = $this->quoteLogiFeeModel->updateInfo($where, $data);*/
 	        
-	        $inquiryModel = $this->inquiryModel;
+	        //$inquiryModel = $this->inquiryModel;
 	        
 	        $logiCheckId = $condition['logi_check_id'];//$this->inquiryModel->getRoleUserId($this->user['group_id'], $inquiryModel::logiCheckRole, 'lg');
 	        
@@ -1010,7 +1010,7 @@ class LogisticsController extends PublicController {
 	 * @author liujf
 	 * @time 2017-08-10
 	 */
-	private function _getTotalQuotePrice($calcuFee, $shippingInsuRate, $calcuRate, $trade = 'CIF', $extRate) {
+	private function _getTotalQuotePrice($calcuFee, $shippingInsuRate, $calcuRate, $trade = 'CIF', $extRate = 1) {
 	    $tmpIfFee = round($calcuFee * 1.1 * $shippingInsuRate / 100 / $calcuRate, 8);
 	    
 	    if ($tmpIfFee >= 8 || $tmpIfFee == 0) {
