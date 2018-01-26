da9b78a9347c228ef6e6b0e29256f9e77a1de06b
wangs <footstep6978@163.com>
2018-01-26 11:23:59 +0800

    attaregg

--- application/V2/models/BuyerAgreement.php
+++ application/V2/models/BuyerAgreement.php
@@ -408,17 +408,18 @@ class BuyerAgreementModel extends PublicModel
     }
     //查看框架协议详情
     public function showAgreeDesc($data){
+        $check=isset($data['check'])?$data['check']:false;
         if(empty($data['execute_no'])){
             return false;
         }
-        $info = $this -> showAgree($data['execute_no']);
+        $info = $this -> showAgree($data['execute_no'],$check);
         if(empty($info)){
             return false;
         }
         return $info;
     }
     //按单号查看数据及附件信息详情-------
-    public function showAgree($execute_no){
+    public function showAgree($execute_no,$check=false){
         $agree = $this->where(array('execute_no'=>$execute_no))->find();
         if(!empty($agree['number'])){
             $agree['number']=(float)$agree['number'];
@@ -432,14 +433,16 @@ class BuyerAgreementModel extends PublicModel
             $attach = new AgreementAttachModel();
             $attachInfo = $attach->field('id,attach_name,attach_url')->where(array('agreement_id'=>$id,'deleted_flag'=>'N'))->select();
             $agree['agree_attach'] = $attachInfo;
-            //组织
-//            $org = new OrgModel();
-//            $orgInfo = $org->getNameById($agree['org_id']);
-//            $agree['org_name'] = $orgInfo;
-            //
-//            $country = new CountryModel();
-//            $countryInfo = $country->getCountryByBn($agree['country_bn'],'zh');
-//            $agree['country_name'] = $countryInfo;
+            if($check==true){
+                //组织
+                $org = new OrgModel();
+                $orgInfo = $org->getNameById($agree['org_id']);
+                $agree['org_name'] = $orgInfo;
+                //
+                $country = new CountryModel();
+                $countryInfo = $country->getCountryByBn($agree['country_bn'],'zh');
+                $agree['country_name'] = $countryInfo;
+            }
         }
         //技术人员名称
         $employee=new EmployeeModel();
