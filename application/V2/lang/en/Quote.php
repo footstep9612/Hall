e2e54dcdde7ff0ebb2755f46bce66e878ed136c6
maimaiti <742163033@qq.com>
2018-01-26 17:26:59 +0800

    报价相关语言包文件

---
+++ application/V2/lang/en/Quote.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * 报价模块接口语言包(英文)
+ * @author 买买提
+ */
+return [
+    'QUOTE_SUCCESS'             =>      'Success',
+    'QUOTE_HAS_RETURNED'        =>      'Unrepeatable return',
+    'QUOTE_RESUBMIT'            =>      'You can not resubmit',
+    'QUOTE_NO_DATA'             =>      'No data',
+    'QUOTE_VALIDATION'          =>      'Successfully validate',
+    'QUOTE_SUPPLIER_REQUIRED'   =>      'Supplier required',
+    'QUOTE_BRAND_REQUIRED'      =>      'Brand required',
+    'QUOTE_PUP_REQUIRED'        =>      'Purchase unit price required',
+    'QUOTE_PUP_NUMBER'          =>      'Purchase unit price must be number',
+    'QUOTE_PUP_NOT_ZERO'        =>      'Purchase unit price must be gt 0',
+    'QUOTE_PPC_REQUIRED'        =>      'Purchase price cur_bn required',
+    'QUOTE_GW_REQUIRED'         =>      'Gross weight required',
+    'QUOTE_GW_NUMBER'           =>      'Gross weight must be number',
+    'QUOTE_PS_REQUIRED'         =>      'Package size required',
+    'QUOTE_PS_NUMBER'           =>      'Package size must be number',
+    'QUOTE_PM_REQUIRED'         =>      'Package mode required',
+    'QUOTE_GS_REQUIRED'         =>      'Goods source required',
+    'QUOTE_SL_REQUIRED'         =>      'Stock loc required',
+    'QUOTE_DD_REQUIRED'         =>      'Delivery days required',
+    'QUOTE_DD_NUMBER'           =>      'Delivery days must be number',
+    'QUOTE_POF_REQUIRED'        =>      'Period of validity required',
+];
+
