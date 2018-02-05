712500844b0ff1f8625637e3049caef423b735ab
liujf <git@liujf>
2018-02-05 10:39:03 +0800

    *

--- application/V2/controllers/Public.php
+++ application/V2/controllers/Public.php
@@ -26,8 +26,8 @@ abstract class PublicController extends Yaf_Controller_Abstract {
         $this->put_data = $this->getPut();
 
         if ($this->getRequest()->getModuleName() == 'V1' &&
-             $this->getRequest()->getControllerName() == 'User' &&
-             in_array($this->getRequest()->getActionName(), ['login', 'register', 'es', 'kafka', 'excel'])) {
+            $this->getRequest()->getControllerName() == 'User' &&
+            in_array($this->getRequest()->getActionName(), ['login', 'register', 'es', 'kafka', 'excel'])) {
             $this->setLang($this->getPut('lang', 'en'));
         } else {
             $this->user = $GLOBALS['SSO_USER'];
