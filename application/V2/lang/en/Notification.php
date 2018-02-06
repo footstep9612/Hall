44fbfcaa2bf3981268a6da8cb79eae9ebb4ab029
maimaiti <742163033@qq.com>
2018-02-06 17:51:02 +0800

    代办列表相关接口语言包

--- application/V2/lang/en/Notification.php
+++ application/V2/lang/en/Notification.php
@@ -6,11 +6,11 @@ return [
     'NOTIFICATION_HOURS'     =>  'Hours ',
     'NOTIFICATION_MINUTES'  =>  'Minutes ',
 
-    'NOTIFICATION_AGENT'            =>  'EN-市场经办人',
-    'NOTIFICATION_CHECK'            =>  'EN-报价审核人',
-    'NOTIFICATION_COUNTRY_AGENT'    =>  'EN-区域负责人',
-    'NOTIFICATION_ERUI'             =>  'EN-易瑞分单员',
-    'NOTIFICATION_ISSUE'            =>  'EN-事业部分单员',
-    'NOTIFICATION_QUOTE'            =>  'EN-报价人'
+    'NOTIFICATION_AGENT'            =>  'Market operator',
+    'NOTIFICATION_CHECK'            =>  'Quote auditor',
+    'NOTIFICATION_COUNTRY_AGENT'    =>  'Area leader',
+    'NOTIFICATION_ERUI'             =>  'Erui leader',
+    'NOTIFICATION_ISSUE'            =>  'Business leader',
+    'NOTIFICATION_QUOTE'            =>  'Quoter'
 
 ];
