ba725549f4e86112b8e4fafb2460dfc7878df33b
wangs <footstep6978@163.com>
2018-03-01 12:01:07 +0800

    del log

--- application/V2/controllers/Orderlog.php
+++ application/V2/controllers/Orderlog.php
@@ -370,7 +370,8 @@ class OrderlogController extends PublicController{
         $results = $OrderLog->deleteData($where);
         //会员升级-start-wnags-订单order_id
         $log=new OrderLogModel();
-        $order=$log->field('order_id')->where(array('id'=>substr($where['id'],0,1)))->find();
+        $crm=explode(',',$where['id'])[0];
+        $order=$log->field('order_id')->where(array('id'=>$crm))->find();
         $param['order_id']=isset($order['order_id'])?$order['order_id']:'';
         $auto=new OrderModel();
         $auto->autoUpgradeByOrder($param);
