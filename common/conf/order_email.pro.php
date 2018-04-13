152e0bc61cd853e31db14b579b9889187ca1cf9e
link <linkai@keruigroup.com>
2018-04-13 17:24:39 +0800

    config

---
+++ common/conf/order_email.pro.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: klp
+ * Date: 2018/1/27
+ * Time: 14:44
+ */
+return[
+    'canada'=> ['zhangyingqi@keruigroup.com', 'tmedeiros@keruigroup.com'],
+    'russia'=> ['sunmk@keruigroup.com','duxin@keruigroup.com'],
+    'kazakhstan'=> ['maofei@keruigroup.com','amanma@keruigroup.com'],
+    'indonesia'=>['hulz@erui.com'],
+    'saudi arabia'=>['xuqf@keruigroup.com']
+];
