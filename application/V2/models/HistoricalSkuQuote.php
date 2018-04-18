7eb3b2320b9a0662f666c7d6595a14bf8a4f6c8d
liujf <git@liujf>
2018-04-18 14:45:26 +0800

    *

--- application/V2/models/HistoricalSkuQuote.php
+++ application/V2/models/HistoricalSkuQuote.php
@@ -117,7 +117,7 @@ class HistoricalSkuQuoteModel extends PublicModel {
     	return $this->getSqlJoint($condition)
                             ->field('a.created_at, '
                                           . ($lang ==  'zh' ? 'b.name_zh' : 'b.name') . ' AS sku_name, b.model,
-                                          c.pn, c.brand, c.quote_qty, c.period_of_validity, c.delivery_days, c.stock_loc, c.purchase_unit_price, c.purchase_price_cur_bn, c.gross_weight_kg, c.package_mode, c.goods_source, c.reason_for_no_quote,
+                                          c.pn, c.brand, c.quote_qty, c.quote_unit, c.period_of_validity, c.delivery_days, c.stock_loc, c.purchase_unit_price, c.purchase_price_cur_bn, c.gross_weight_kg, c.package_mode, c.package_size, c.goods_source, c.reason_for_no_quote,
                                           d.sku, d.supplier_id,
                                           e.name'. ($lang == 'zh' ? '' : '_en') . ' AS supplier_name,
                                           CASE
