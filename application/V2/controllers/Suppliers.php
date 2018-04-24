e0fe6dc36fec04e2b6bca4f0c1b18f7cbc2f0ece
mmt <maimt@keruigroup.com>
2018-04-24 10:22:17 +0800

    【新版供应商】fix 供应商-详情接口-国家简称显示问题

--- application/V2/controllers/Suppliers.php
+++ application/V2/controllers/Suppliers.php
@@ -423,6 +423,9 @@ class SuppliersController extends PublicController {
 
         $res = $this->suppliersModel->getJoinDetail($condition);
 
+        //国家
+        $res['country_bn'] = (new CountryModel)->getCountryNameByBn($res['country_bn']);
+
         //供应商的品牌(对象)
         $res['brand'] = (new SupplierBrandModel)->brandsObjectBy($condition['id']);
 
