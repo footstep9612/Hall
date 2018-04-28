87aa1ec0d6f4331184ed1020c70be42444a60660
azhong19820504 <297238770@qq.com>
2018-04-28 16:02:53 +0800

    ES
    Signed-off-by: azhong19820504 <297238770@qq.com>

--- application/V2/controllers/Esproduct.php
+++ application/V2/controllers/Esproduct.php
@@ -28,8 +28,6 @@ class EsproductController extends PublicController {
         } else {
             parent::init();
         }
-        ini_set("display_errors", "On");
-        error_reporting(E_ALL | E_STRICT);
     }
 
     public function clearCacheAction() {
