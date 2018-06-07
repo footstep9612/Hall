e3efacc5336113a3db014359e5743a38743e1a40
wangs <footstep6978@163.com>
2018-06-07 17:54:00 +0800

    sub

--- application/V2/controllers/Buyerbusiness.php
+++ application/V2/controllers/Buyerbusiness.php
@@ -446,6 +446,21 @@ class BuyerbusinessController extends PublicController
         }
         $this -> jsonReturn($dataJson);
     }
+    //提交
+    public function submitGradeAction(){
+        $data = json_decode(file_get_contents("php://input"), true);
+        $data['created_by']=$this->user['id'];
+        $model = new CustomerGradeModel();  //结算方式
+        $res=$model->submitGrade($data);
+        if($res){
+            $dataJson['code']=1;
+            $dataJson['message']='成功';
+        }else{
+            $dataJson['code']=0;
+            $dataJson['message']='失败';
+        }
+        $this -> jsonReturn($dataJson);
+    }
     //审核
     public function checkedGradeAction(){
         $data = json_decode(file_get_contents("php://input"), true);
