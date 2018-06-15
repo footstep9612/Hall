5b690ef953d4e458c52dc6057b382b92cc743a7d
mmt <maimt@keruigroup.com>
2018-06-15 14:54:23 +0800

    [瑞商产品]-产品详情接口-商品自定义属性

--- application/V2/models/SupplierGoodsAttr.php
+++ application/V2/models/SupplierGoodsAttr.php
@@ -13,6 +13,8 @@ class SupplierGoodsAttrModel extends PublicModel
     public function getAttr($request)
     {
         $attrs = $this->where(['spu' => $request['spu'], 'sku' => $request['sku'], 'deleted_flag' => 'N'])->getField('ex_goods_attrs');
-        return explode(',', $attrs);
+        //$arr = json_decode($attrs,true);
+        //p($arr);
+        return json_decode($attrs,true);
     }
 }
