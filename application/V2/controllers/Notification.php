2010cf6c0ca39cb305d240c7dd2a28365ae558d5
liujf <git@liujf>
2018-06-19 19:01:48 +0800

    新增首页快捷入口接口

--- application/V2/controllers/Notification.php
+++ application/V2/controllers/Notification.php
@@ -42,13 +42,17 @@ class NotificationController extends PublicController
             $item['area_name'] = $marketAreaModel->getAreaNameByBn($item['area_bn'], $this->lang);
             $item['country_name'] = $countryModel->getCountryNameByBn($item['country_bn'], $this->lang);
         }
-
-        $this->jsonReturn([
-            'code'    => 1,
-            'message' => L('NOTIFICATION_SUCCESS'),
-            'count'   => count($inquiry->where($where)->field('id')->select()),
-            'data'    => $list
-        ]);
+        
+        if ($list) {
+            $res['code'] = '1';
+            $res['message'] = L('SUCCESS');
+            $res['count'] = $inquiry->where($where)->count('id') ? : 0;
+            $res['data'] = $list;
+        } else {
+            $res['code'] = '-101';
+            $res['message'] = L('NO_DATA');
+        }
+        $this->jsonReturn($res);
 
     }
     
