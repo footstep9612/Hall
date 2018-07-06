59500446f06044643f5f4be2c0d5f5db553356c6
azhong19820504 <297238770@qq.com>
2018-07-06 11:23:54 +0800

    Notification.php
    Signed-off-by: azhong19820504 <297238770@qq.com>

--- application/V2/controllers/Notification.php
+++ application/V2/controllers/Notification.php
@@ -18,9 +18,7 @@ class NotificationController extends PublicController {
         $pagesize = !empty($request['pageSize']) ? $request['pageSize'] : 10;
 
         $inquiry = new InquiryModel();
-        $marketAreaCountryModel = new MarketAreaCountryModel();
-        $marketAreaModel = new MarketAreaModel();
-        $countryModel = new CountryModel();
+
         $urlPermModel = new UrlPermModel();
         $where = [
             'now_agent_id' => $this->user['id'],
@@ -32,21 +30,22 @@ class NotificationController extends PublicController {
                 ->field('id,serial_no,inflow_time,status,quote_status,country_bn')
                 ->page($page, $pagesize)
                 ->select();
-
+        $this->_setAreaBn($list);
+        $this->_setArea($list);
+        $this->_setCountry($list);
+        $this->_remindList($list);
         foreach ($list as &$item) {
-            $item['remind_count'] = count($this->remindList($item['id']));
-            $item['remind_list'] = $this->remindList($item['id']);
             $item['inflow_time'] = $this->_setInflowTime($item['inflow_time']);
-            $item['area_bn'] = $marketAreaCountryModel->where(['country_bn' => $item['country_bn']])->getField('market_area_bn');
-            $item['area_name'] = $marketAreaModel->getAreaNameByBn($item['area_bn'], $this->lang);
-            $item['country_name'] = $countryModel->getCountryNameByBn($item['country_bn'], $this->lang);
         }
 
         if ($list) {
             $res['code'] = '1';
             $res['message'] = L('SUCCESS');
-            $res['count'] = $inquiry->where($where)->count('id') ?: 0;
-            $res['parent_id'] = $urlPermModel->getMenuIdByName('询报价');
+            $res['count'] = $inquiry
+                            ->where($where)
+                            ->count('id') ?: 0;
+            $res['parent_id'] = $urlPermModel
+                    ->getMenuIdByName('询报价');
             $res['data'] = $list;
         } else {
             $res['code'] = '-101';
@@ -73,16 +72,15 @@ class NotificationController extends PublicController {
             $currentPage = empty($request['currentPage']) ? 1 : $request['currentPage'];
             $pageSize = empty($request['pageSize']) ? 10 : $request['pageSize'];
             $where = ['status' => 'APPROVING', 'country_bn' => ['in', $this->user['country_bn'] ?: ['-1']], 'deleted_flag' => 'N'];
-            $buyerList = $buyerModel->field('id, status, country_bn, name')
+            $buyerList = $buyerModel
+                    ->field('id, status, country_bn, name')
                     ->where($where)
                     ->page($currentPage, $pageSize)
                     ->order('id DESC')
                     ->select();
-            foreach ($buyerList as &$buyer) {
-                $buyer['area_bn'] = $marketAreaCountryModel->where(['country_bn' => $buyer['country_bn']])->getField('market_area_bn');
-                $buyer['area_name'] = $marketAreaModel->getAreaNameByBn($buyer['area_bn'], $this->lang);
-                $buyer['country_name'] = $countryModel->getCountryNameByBn($buyer['country_bn'], $this->lang);
-            }
+            $this->_setAreaBn($buyerList);
+            $this->_setArea($buyerList);
+            $this->_setCountry($buyerList);
             if ($buyerList) {
                 $res['code'] = '1';
                 $res['message'] = L('SUCCESS');
@@ -184,12 +182,58 @@ class NotificationController extends PublicController {
 
         $where = ['inquiry_id' => $inquiry_id, 'isread_flag' => 'N'];
         $data = $inquiryRemind->where($where)->field('inquiry_id, created_by, created_at')->select();
-
         foreach ($data as &$datum) {
             $datum['role_name'] = $this->_setRoleName($inquiry->getUserRoleById($datum['created_by']));
             $datum['created_by'] = $this->_setUserName($datum['created_by']);
         }
 
+        return $data;
+    }
+
+    public function _remindList(&$list) {
+
+        if ($list) {
+            $inquiryRemind = new InquiryRemindModel();
+            $inquiry = new InquiryModel();
+            $inquiry_ids = [];
+            foreach ($list as $item) {
+                $inquiry_ids[] = $item['id'];
+            }
+
+            $where = ['inquiry_id' => ['in', $inquiry_ids], 'isread_flag' => 'N'];
+            $data = $inquiryRemind->where($where)->field('inquiry_id, created_by, created_at')->select();
+            $ret = [];
+            foreach ($data as &$datum) {
+                $datum['role_name'] = $this->_setRoleName($inquiry->getUserRoleById($datum['created_by']));
+                $datum['created_by'] = $this->_setUserName($datum['created_by']);
+                $ret[$datum['inquiry_id']][] = $datum;
+            }
+            foreach ($list as $key => $item) {
+                if (!empty($ret[$item['id']])) {
+
+                    $item['remind_count'] = count($ret[$item['id']]);
+                    $item['remind_list'] = $ret[$item['id']];
+                } else {
+
+                    $item['remind_count'] = 0;
+                    $item['remind_list'] = [];
+                }
+                $list[$key] = $item;
+            }
+            return $data;
+        }
+    }
+
+    public function remindCount($inquiry_id) {
+
+        $inquiryRemind = new InquiryRemindModel();
+        $where = ['isread_flag' => 'N'];
+        if ($inquiry_id && is_numeric($inquiry_id)) {
+            $where = ['inquiry_id' => $inquiry_id];
+        } elseif ($inquiry_id && is_array($inquiry_id)) {
+            $where = ['inquiry_id' => ['in', $inquiry_id]];
+        }
+        $data = $inquiryRemind->where($where)->count();
         return $data;
     }
 
@@ -229,6 +273,100 @@ class NotificationController extends PublicController {
 
         $distance_str = $day . L('NOTIFICATION_DAYS') . $hour . L('NOTIFICATION_HOURS') . $minut . L('NOTIFICATION_MINUTES');
         return $distance_str;
+    }
+
+    /*
+     * Description of 获取国家
+     * @param array $arr
+     * @author  zhongyg
+     * @date    2017-8-2 13:07:21
+     * @version V2.0
+     * @desc
+     */
+
+    private function _setCountry(&$arr) {
+        if ($arr) {
+            $country_model = new CountryModel();
+            $country_bns = [];
+            foreach ($arr as $key => $val) {
+                $country_bns[] = trim($val['country_bn']);
+            }
+            $countrynames = $country_model->getNamesBybns($country_bns, $this->lang);
+            foreach ($arr as $key => $val) {
+                if (trim($val['country_bn']) && isset($countrynames[trim($val['country_bn'])])) {
+                    $val['country_name'] = $countrynames[trim($val['country_bn'])];
+                } else {
+                    $val['country_name'] = '';
+                }
+                $arr[$key] = $val;
+            }
+        }
+    }
+
+    /*
+     * Description of 获取国家
+     * @param array $arr
+     * @author  zhongyg
+     * @date    2017-8-2 13:07:21
+     * @version V2.0
+     * @desc
+     */
+
+    private function _setAreaBn(&$arr) {
+        if ($arr) {
+            $marketAreaCountryModel = new MarketAreaCountryModel();
+            $country_bns = [];
+            foreach ($arr as $key => $val) {
+                $country_bns[] = trim($val['country_bn']);
+            }
+            $area_bns = $marketAreaCountryModel
+                            ->field('country_bn,market_area_bn')
+                            ->where(['country_bn' => ['in', $country_bns]])->select();
+
+
+            $countrytoarea_bns = [];
+            foreach ($area_bns as $item) {
+                $countrytoarea_bns[$item['country_bn']] = $item['market_area_bn'];
+            }
+
+            foreach ($arr as $key => $val) {
+                if (trim($val['country_bn']) && isset($countrytoarea_bns[trim($val['country_bn'])])) {
+                    $val['area_bn'] = $countrytoarea_bns[trim($val['country_bn'])];
+                } else {
+                    $val['area_bn'] = '';
+                }
+
+                $arr[$key] = $val;
+            }
+        }
+    }
+
+    /*
+     * Description of 获取营销区域
+     * @param array $arr
+     * @author  zhongyg
+     * @date    2017-8-2 13:07:21
+     * @version V2.0
+     * @desc
+     */
+
+    private function _setArea(&$arr) {
+        if ($arr) {
+            $marketarea_model = new MarketAreaModel();
+            $area_bns = [];
+            foreach ($arr as $key => $val) {
+                $area_bns[] = trim($val['area_bn']);
+            }
+            $area_names = $marketarea_model->getNamesBybns($area_bns);
+            foreach ($arr as $key => $val) {
+                if (trim($val['area_bn']) && isset($area_names[trim($val['area_bn'])])) {
+                    $val['area_name'] = $area_names[trim($val['area_bn'])];
+                } else {
+                    $val['area_name'] = '';
+                }
+                $arr[$key] = $val;
+            }
+        }
     }
 
 }
