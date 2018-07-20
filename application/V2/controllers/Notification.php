75bf282d9592c9a3221220afe5ece6afb532deab
azhong19820504 <297238770@qq.com>
2018-07-20 10:50:11 +0800

    数据报错
    Signed-off-by: azhong19820504 <297238770@qq.com>

--- application/V2/controllers/Notification.php
+++ application/V2/controllers/Notification.php
@@ -72,7 +72,7 @@ class NotificationController extends PublicController {
             $currentPage = empty($request['currentPage']) ? 1 : $request['currentPage'];
             $pageSize = empty($request['pageSize']) ? 10 : $request['pageSize'];
             $where = ['status' => 'APPROVING', 'country_bn' => ['in', $this->user['country_bn'] ?: ['-1']],
-                'and `name` is not null and `name`<>\'\'',
+                '`name` is not null and `name`<>\'\'',
                 'deleted_flag' => 'N'];
             $buyerList = $buyerModel
                     ->field('id, status, country_bn, name')
@@ -114,7 +114,7 @@ class NotificationController extends PublicController {
             $where = ['status' => 'APPROVING',
                 'country_bn' => ['in',
                     $this->user['country_bn']]
-                , 'and `name` is not null and `name`<>\'\'', 'deleted_flag' => 'N'];
+                , '`name` is not null and `name`<>\'\'', 'deleted_flag' => 'N'];
             if (in_array(InquiryModel::inquiryIssueRole, $role_nos) || in_array(InquiryModel::quoteIssueMainRole, $role_nos)) {
                 if ($this->user['group_id']) {
                     $map1 = [];
