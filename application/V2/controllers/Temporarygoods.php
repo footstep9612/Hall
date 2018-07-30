c2638021d86a7f2d627190c1c286139938f56e25
mmt <maimt@keruigroup.com>
2018-07-30 14:46:15 +0800

    [bug fix]-商品-询报价临时SKU（审核人字段）

--- application/V2/controllers/Temporarygoods.php
+++ application/V2/controllers/Temporarygoods.php
@@ -58,29 +58,14 @@ class TemporarygoodsController extends PublicController {
      */
 
     private function _setUname(&$response) {
-        foreach ($response as $item) {
-            if ($item['checked_by']) {
-                $user_ids[] = $item['checked_by'];
-            }
-        }
-        if (!empty($user_ids)) {
-            $employee_model = new EmployeeModel();
-            $usernames = $employee_model->getUserNamesByUserids($user_ids);
-            foreach ($response as $key => $val) {
 
-                if ($val['checked_by'] && isset($usernames[$val['checked_by']])) {
-                    $val['checked_by_name'] = $usernames[$val['checked_by']];
-                } else {
-                    $val['checked_by_name'] = '';
-                }
+        $inquiry = new InquiryModel();
+        $employye = new EmployeeModel();
 
-                $response[$key] = $val;
-            }
-        } else {
-            foreach ($response as $key => $val) {
-                $val['checked_by_name'] = '';
-                $response[$key] = $val;
-            }
+        foreach ($response as $key=>$val) {
+            $check_org_id = $inquiry->where(['id' => $val['inquiry_id']])->getField('check_org_id');
+            $val['checked_by_name'] = $employye->getNameByid($check_org_id)['name'];
+            $response[$key] = $val;
         }
     }
 
