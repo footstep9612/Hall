aed456426040ca1fe1903c30775e8a8bddd256a5
azhong19820504 <297238770@qq.com>
2018-08-01 13:43:31 +0800

    毛重体积 有效期 Signed-off-by: azhong19820504 <297238770@qq.com>

--- application/V2/models/QuoteItem.php
+++ application/V2/models/QuoteItem.php
@@ -241,7 +241,7 @@ class QuoteItemModel extends PublicModel {
             }
 
             // 校验必填字段，如果有未填项且主键id为空就跳过，否则删除该记录
-            if ($value['name'] == '' || $value['name_zh'] == '' || $value['qty'] == '' || $value['unit'] == '' || $value['brand'] == '' || $value['purchase_unit_price'] == '' || $value['purchase_price_cur_bn'] == '' || $value['gross_weight_kg'] == '' || $value['package_mode'] == '' || $value['package_size'] == '' || $value['stock_loc'] == '' || $value['goods_source'] == '' || $value['delivery_days'] == '' || $value['period_of_validity'] == '' || (empty($value['category']) && $is_erui == 'N')) {
+            if ($value['name'] == '' || $value['name_zh'] == '' || $value['qty'] == '' || $value['unit'] == '' || $value['brand'] == '' || $value['purchase_unit_price'] == '' || $value['purchase_price_cur_bn'] == '' || $value['package_mode'] == '' || $value['stock_loc'] == '' || $value['goods_source'] == '' || $value['delivery_days'] == '' || (empty($value['category']) && $is_erui == 'N')) {
                 if ($value['id'] == '') {
                     continue;
                 } else {
