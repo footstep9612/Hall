70df83a9559cfadab35062ac00b141d45beaa7a8
azhong19820504 <297238770@qq.com>
2018-08-09 10:08:21 +0800

    防止重复点击
    Signed-off-by: azhong19820504 <297238770@qq.com>

--- application/V2/models/Rfq/CheckLog.php
+++ application/V2/models/Rfq/CheckLog.php
@@ -65,6 +65,9 @@ class Rfq_CheckLogModel extends PublicModel {
                 }
                 $log ['out_node'] = $in_node;
             }
+            if ($log['out_node'] == $out_node) {
+                return false;
+            }
             $data = [
                 'inquiry_id' => $inquiry_id,
                 'action' => $action,
@@ -87,6 +90,10 @@ class Rfq_CheckLogModel extends PublicModel {
     static public function AddQuoteToSubmitLog($inquiry_id, $user) {
         try {
             $log = self::Instance()->field('out_at,out_node')->where(['inquiry_id' => $inquiry_id])->order('created_at desc')->find();
+            if ($log['out_node'] == 'MARKET_APPROVING') {
+                return false;
+            }
+
             $data = [
                 'inquiry_id' => $inquiry_id,
                 'action' => 'CREATE',
