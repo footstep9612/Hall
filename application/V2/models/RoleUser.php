109ff5c07fbf44e890a9e5d900b49f9090e1d731
azhong19820504 <297238770@qq.com>
2018-08-09 10:42:16 +0800

    测试
    Signed-off-by: azhong19820504 <297238770@qq.com>

--- application/V2/models/RoleUser.php
+++ application/V2/models/RoleUser.php
@@ -305,7 +305,7 @@ class RoleUserModel extends PublicModel {
                 $where['fp.parent_id'] = 0;
             }
             if (!empty($not_pid)) {
-                $where['fp.parent_id'] = ['neq', $not_pid];
+                $where[] = 'fp.parent_id <>' . $not_pid;
             }
             if ($source) {
                 $where['fp.source'] = $source;
