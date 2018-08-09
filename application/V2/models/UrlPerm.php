279385d8db8bf884a20213d19d27f78193e8c6a4
azhong19820504 <297238770@qq.com>
2018-08-09 10:56:50 +0800

    首页
    Signed-off-by: azhong19820504 <297238770@qq.com>

--- application/V2/models/UrlPerm.php
+++ application/V2/models/UrlPerm.php
@@ -215,7 +215,7 @@ class UrlPermModel extends PublicModel {
                 redisSet('HOME_ID', $parent_id);
             }
             $data = $this->getlist(['id' => $parent_id], null);
-            $data['func_perm_id'] = $data['id'];
+            $data[0]['func_perm_id'] = $data['id'];
             redisSet('HOME', json_encode($data));
             return $data;
         }
